5f37dd8658a1a6a579cb9b82f014bb7e4601fc63
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2022-04-07 15:02:24 -0700

    check Container's rows step for Bulk

--- src/main/java/prodsuppqa/my/salesforce/com/Pages/SupplyConsolePage.java
+++ src/main/java/prodsuppqa/my/salesforce/com/Pages/SupplyConsolePage.java
@@ -1,14 +1,65 @@
 package prodsuppqa.my.salesforce.com.Pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class SupplyConsolePage extends BasePage {
-    @FindBy(id = "Automation Supply Location_1")
-    private WebElement clinician_prodsuppqa;
+    @FindBy(xpath = ".//span[text() = 'Supply Locations']")
+    private WebElement supply_locations_tab;
+    private By supply_locations_tab1 = By.xpath("(.//a[@title = 'Supply Locations'])");
+
+    @FindBy(linkText = "Automation Supply Location_1")
+    private WebElement supply_supply_location_1;
+    private By supply_supply_location_1_ = By.linkText("Automation Supply Location_1");
+
+    @FindBy(xpath = "(//table[@class = 'slds-table slds-table_header-fixed slds-table_bordered slds-table_edit']/tbody/tr)")
+    private WebElement rows_count_path;
+    private By rows_count_path_1 = By.xpath("(//table[@class = 'slds-table slds-table_header-fixed slds-table_bordered slds-table_edit']/tbody/tr)");
+
+    //@FindBy(xpath = ".//span[text() = 'Select Item 1']")
+    //@FindBy(xpath = "(.//span[@class = 'slds-checkbox_faux'])[2]")
+    //private WebElement container_checkbox_1;
+    //private By container_checkbox_1_ = By.xpath("(.//span[@class = 'slds-checkbox_faux'])[2]");
+
 
     public SupplyConsolePage(WebDriver driver) {
         super(driver);
     }
+
+    public void clickSupplyLocationsTab() throws InterruptedException {
+        waitForElementToBeLocated(driver, supply_locations_tab1, 10);
+        WebElement element = driver.findElement(supply_locations_tab1);
+        //waitForElementToBeVisible(driver, element, 10);
+        //JavascriptExecutor executor = (JavascriptExecutor) driver;
+        //executor.executeScript("arguments[0].click();", element);
+        //click(supply_locations_tab1);
+        this.supply_locations_tab.click();
+    }
+
+    public void clickOnSupplyLocations() throws InterruptedException {
+        waitForElementToBeLocated(driver, supply_supply_location_1_, 10);
+        WebElement element = driver.findElement(supply_supply_location_1_);
+        this.supply_supply_location_1.click();
+    }
+
+    public void clickOnCheckbox(int k) throws InterruptedException {
+        By container_checkbox_1_ = By.xpath("(.//span[@class = 'slds-checkbox_faux'])["+ k +"]");
+        waitForElementToBeLocated(driver, container_checkbox_1_, 10);
+        //waitForElementToBeLocated(driver, container_checkbox_1_, 10);
+        WebElement element = driver.findElement(container_checkbox_1_);
+        //this.container_checkbox_1.click();
+        click(container_checkbox_1_);
+    }
+
+    public int getRowsCount() throws InterruptedException {
+        //waitForElementToBeClickable(driver, container_checkbox_1, 10);
+        //waitForElementToBeLocated(driver, container_checkbox_1_, 10);
+        List<WebElement> rows = driver.findElements(rows_count_path_1);
+        //rows.size();
+        return(rows.size());
+    }
+
+
 }
--- src/test/java/prodsuppqa/my/salesforce/com/Tests/Inventory/BulkTransfers.java
+++ src/test/java/prodsuppqa/my/salesforce/com/Tests/Inventory/BulkTransfers.java
@@ -1,6 +1,8 @@
 package prodsuppqa.my.salesforce.com.Tests.Inventory;
 
+import org.openqa.selenium.Dimension;
 import org.testng.annotations.Test;
+import prodsuppqa.my.salesforce.com.Pages.InClinicExperiencePage;
 import prodsuppqa.my.salesforce.com.Pages.SupplyConsolePage;
 import prodsuppqa.my.salesforce.com.Tests.BaseTest;
 
@@ -12,8 +14,32 @@ public class BulkTransfers extends BaseTest {
     public void Can_do_Bulk_transfers_as_PPHIS_PRODSUPPQA() throws InterruptedException {
         System.out.println("/*----Login as an PPHIS_prodsuppqa to Supply Console --*/");
         SupplyConsolePage supplyConsolePagePage = loginPage.loginAsPPHIS();
-        sleep(10000);
-        System.out.println("/*----Login as an PPHIS_prodsuppqa to Supply Console --*/");
+        System.out.println("/*----Go to Supply Locations Tab --*/");
+        supplyConsolePagePage.clickSupplyLocationsTab();
+        //Thread.sleep(10000);
+        System.out.println("/*----Click on Automation Supply Location_1 --*/");
+        supplyConsolePagePage.clickOnSupplyLocations();
+        Thread.sleep(5000);
+        System.out.println("/*----Get count records --*/");
+        int count = supplyConsolePagePage.getRowsCount();
+        System.out.println("count:" +count);
+        System.out.println("/*----Click on Containers records Checkbox --*/");
+        if (count >= 3){
+            int k=2;
+            while (k <= 4){
+                supplyConsolePagePage.clickOnCheckbox(k);
+                System.out.println("/*--- record number --*/" +k);
+                Thread.sleep(5000);
+                k++;
+            }
+        }else {
+            System.out.println("/*--not enough records for Bulk actions--*/");
+        }
+
+        //Thread.sleep(10000);
+
+
+
 
     }
 
