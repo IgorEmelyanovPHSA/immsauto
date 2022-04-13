fabafb8edab2806758e6b50905d2cde06feabffb
annesunil <sunil.anne@phsa.ca>
2022-04-13 15:22:30 -0700

    Update logic to handle to delete multiple records

--- src/main/java/prodsuppqa/my/salesforce/com/Pages/InClinicExperiencePage.java
+++ src/main/java/prodsuppqa/my/salesforce/com/Pages/InClinicExperiencePage.java
@@ -9,8 +9,8 @@ public class InClinicExperiencePage extends BasePage {
     @FindBy(xpath = ".//span[text() = 'Register']")
     private WebElement register_tab;
     
-    //@FindBy(xpath = ".//a[@title = 'Register']")
-    //private WebElement register_tab;
+   /* @FindBy(xpath = ".//a[@title = 'Register']")
+    private WebElement register_tab;*/
     
     private By register_tab1 = By.xpath("(.//a[@title = 'Register'])");
     
@@ -34,12 +34,7 @@ public class InClinicExperiencePage extends BasePage {
     private WebElement click_related_tab;
     private By click_related_tab1 = By.xpath("//a[@data-label='Related']");
     
-    
-    
-    
-    
     @FindBy(xpath = "(//A[@title='Show 2 more actions'])")
-    
     private WebElement select_Imms_record;
     private By select_Imms_record1 = By.xpath("/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___-b-c-h_-participant_-record_-page___-account___-v-i-e-w/forcegenerated-flexipage_bch_participant_record_page_account__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-template-desktop2/div/div[2]/div[1]/slot/flexipage-component2/slot/flexipage-tabset2/div/lightning-tabset/div/slot/slot/flexipage-tab2[2]/slot/flexipage-component2[6]/slot/lst-related-list-container/div/div[2]/lst-related-list-single-container/laf-progressive-container/slot/lst-related-list-single-app-builder-mapper/article/lst-related-list-view-manager/lst-common-list-internal/div/div/lst-primary-display-manager/div/lst-primary-display/lst-primary-display-grid/lst-customized-datatable/div[2]/div/div/table/tbody/tr/th/lightning-primitive-cell-factory/span/div/lightning-primitive-custom-cell/force-lookup/div/records-hoverable-link/div/a/slot/slot/span");
     
@@ -63,6 +58,7 @@ public class InClinicExperiencePage extends BasePage {
     @FindBy(xpath = "//BUTTON[@name='Delete'][text()='Delete']")
     private WebElement delete_rern_record;
     private By delete_rern_record1 = By.xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___flexipage__default_rec_-l___-d-d-h__-h-c_-rules_-engine_-response__c___-v-i-e-w/forcegenerated-flexipage_default_rec_l_ddh__hc_rules_engine_response__c__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-template-desktop2/div/div[1]/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_ddh__hc_rules_engine_response__c___012000000000000aaa___compact___view___recordlayout2/records-highlights2/div[1]/div/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[2]/runtime_platform_actions-action-renderer/runtime_platform_actions-executor-aura-legacy/slot/slot/lightning-button/button");
+    
     @FindBy(xpath = "//button[text()='Delete']")
     private WebElement delete_person_account;
     private By delete_person_account1 = By.xpath("//button[text()='Delete']");
@@ -200,5 +196,9 @@ public class InClinicExperiencePage extends BasePage {
         Thread.sleep(2000);
     }
     
+    public void closeDeletedPersonAccount() {
+        WebElement products = driver.findElement(By.xpath("//*[@data-key='close'][@class='slds-icon slds-icon-text-default slds-icon_xx-small']"));
+        products.click();
+    }
 }
 
