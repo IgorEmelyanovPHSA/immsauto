304b58d31f5e76d1f21a657807d84e53f6b105fe
annesunil <sunil.anne@phsa.ca>
2022-04-20 12:21:01 -0700

    Updated code to handle frame window

--- src/test/java/prodsuppqa/my/salesforce/com/Tests/Inventory/Requisition.java
+++ src/test/java/prodsuppqa/my/salesforce/com/Tests/Inventory/Requisition.java
@@ -1,39 +1,33 @@
 package prodsuppqa.my.salesforce.com.Tests.Inventory;
 
 
-
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import prodsuppqa.my.salesforce.com.Pages.RequisitionFlowPage;
-import prodsuppqa.my.salesforce.com.Pages.SupplyConsolePage;
 import prodsuppqa.my.salesforce.com.Tests.BaseTest;
 
-import static java.lang.Thread.*;
-
 public class Requisition extends BaseTest {
-
+	
 	@Test
 	public void Create_Requisition_as_PPHIS_PRODSUPPQA() throws InterruptedException {
-
-		System.out.println("/*----1. Login as an PPHIS_prodsuppqa to Supply Console --*/");
+		
 		RequisitionFlowPage requisitionFlowPage = loginPage.loginAsPPHIS1();
-//        System.out.println("/*----000. Close Other Tabs --*/");
-//        requisitionFlowPage.closeOtherTabs();
-//        Thread.sleep(4000);
-//        System.out.println("/*----2. Locate Dropdown Menu --*/");
-		Thread.sleep(4000);
-		requisitionFlowPage.clickDropdownMenu();
-		Thread.sleep(6000);
-		System.out.println("/*----3. Navigate to Supply Locations --*/");
-		requisitionFlowPage.clickSupplyLocationInDropdown();
-		Thread.sleep(6000);
-		System.out.println("/*----3. Locate on Automation Supply Location_1 --*/");
-		requisitionFlowPage.clickSupplyLocationName();
-		Thread.sleep(4000);
-		System.out.println("/*----4. Navigate to Request Supplies --*/");
-		requisitionFlowPage.clickRequestSupplies();
+		System.out.println("/*----1. Login as an PPHIS_prodsuppqa to Supply Console --*/");
 		Thread.sleep(3000);
-		System.out.println("/*----5. Choose Ship To Address --*/");
+		requisitionFlowPage.closeTab();
+		System.out.println("/*----2. Close open Tabs --*/");
+		Thread.sleep(3000);
+		requisitionFlowPage.clickDropdownMenu();
+		System.out.println("/*----3. Locate and click on Dropdown Menu --*/");
+		Thread.sleep(3000);
+		requisitionFlowPage.clickSupplyLocationInDropdown();
+		System.out.println("/*----4. Navigate to Supply Locations --*/");
+		Thread.sleep(3000);
+		requisitionFlowPage.clickSupplyLocationName();
+		System.out.println("/*----5. Locate on Automation Supply Location_1 --*/");
+		Thread.sleep(3000);
+		requisitionFlowPage.clickRequestSupplies();
+		System.out.println("/*----6. Navigate to Request Supplies --*/");
+		Thread.sleep(3000);
 		//requisitionFlowPage.clickShipToAddress();
 		Thread.sleep(3000);
 		System.out.println("/*----6. Locate on Automation Supply Location_2 --*/");
@@ -82,12 +76,12 @@ public class Requisition extends BaseTest {
 		Thread.sleep(3000);
 		System.out.println("/*----19. Approve Requisition--*/");
 		requisitionFlowPage.clickApproveRequisition();
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		System.out.println("/*----20. Select Supply Container With Entering Approved Request Dose--*/");
 		requisitionFlowPage.enterApprovedDose("1");
 		Thread.sleep(9000);
-
-
+		//requisitionFlowPage.closeTabs();
+		
 	}
-
+	
 }
