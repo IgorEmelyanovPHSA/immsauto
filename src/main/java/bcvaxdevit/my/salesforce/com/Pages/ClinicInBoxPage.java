052aae193ff8750ee176d078fbd0c33b57c94bef
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2022-04-25 14:10:29 -0700

    add closeAllTabs to ClinicInBox page

--- src/main/java/bcvaxdevit/my/salesforce/com/Pages/ClinicInBoxPage.java
+++ src/main/java/bcvaxdevit/my/salesforce/com/Pages/ClinicInBoxPage.java
@@ -1,15 +1,41 @@
 package bcvaxdevit.my.salesforce.com.Pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 
 public class ClinicInBoxPage extends BasePage {
 
-    @FindBy(xpath = ".//span[text() = 'Register']")
+    @FindBy(xpath = ".//button[text() = 'Register New Citizen']")
     private WebElement register_button;
+    private By register_button_1 = By.xpath(".//button[text() = 'Register New Citizen']");
+
+    @FindBy(xpath = ".//span[@title='Clinic in a Box (IPM)']")
+    private WebElement clinicinbox_page_displayed;
 
     public ClinicInBoxPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickRegisterButton() throws InterruptedException {
+        waitForElementToBeVisible(driver, register_button, 10);
+        WebElement element = driver.findElement(register_button_1);
+        register_button.click();
+    }
+
+    public void closeAllTabs() throws InterruptedException {
+        do {
+            try {
+                WebElement closetab = driver.findElement(By.xpath("(.//button[@class = 'slds-button slds-button_icon slds-button_icon-x-small slds-button_icon-container'])"));
+                closetab.click();
+                Thread.sleep(5000);
+            } catch (NoSuchElementException e) {
+                System.out.println("/*---there are no Tab's to close anymore");
+            }
+        } while (isDisplayed(By.xpath("(.//button[@class = 'slds-button slds-button_icon slds-button_icon-x-small slds-button_icon-container'])")));
+    }
+
+    public void verifyIsClinicInBoxPageDisplayed() {
+        waitForElementToBeVisible(driver, clinicinbox_page_displayed, 10);
+        this.clinicinbox_page_displayed.isDisplayed();
     }
 }
--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/BulkTransfers.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/BulkTransfers.java
@@ -13,22 +13,22 @@ public class BulkTransfers extends BaseTest {
         System.out.println("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
         SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHIS();
         Thread.sleep(5000);
-        System.out.println("/*2.----Go to Supply Locations Tab --*/");
-        Thread.sleep(5000);
         //Assert.assertTrue(false);
+        System.out.println("/*2.----Supply Console Page displayed --*/");
         supplyConsolePage.verifyIsSupplyPageDisplayed();
         Thread.sleep(5000);
         System.out.println("/*3.----Close All previously opened Tab's --*/");
         supplyConsolePage.closeAutomationLocationTab();
         Thread.sleep(2000);
+        System.out.println("/*4.----Go to Supply Locations Tab --*/");
         supplyConsolePage.clickSupplyLocationsTab();
-        System.out.println("/*4.----Click on Automation Supply Location_1 --*/");
+        System.out.println("/*5.----Click on Automation Supply Location_1 --*/");
         supplyConsolePage.clickOnSupplyLocation_1();
         Thread.sleep(5000);
-        System.out.println("/*5.----Get Supply Containers count outcoming records --*/");
+        System.out.println("/*6.----Get Supply Containers count outcoming records --*/");
         int countSupplyContainers = supplyConsolePage.getRowsSupplyContainersFromCount();
         System.out.println("/*---     count:" +countSupplyContainers);
-        System.out.println("/*6.----Click on Container's records Checkboxes --*/");
+        System.out.println("/*7.----Click on Container's records Checkboxes --*/");
         if (countSupplyContainers >= 3){
             int k=1;
             while (k <= 3){
@@ -41,10 +41,10 @@ public class BulkTransfers extends BaseTest {
         }else {
             System.out.println("/*--not enough records for Bulk actions--*/");
         }
-        System.out.println("/*7.----Click on bulk Transfer button --*/");
+        System.out.println("/*8.----Click on bulk Transfer button --*/");
         supplyConsolePage.clickBulkTransfersButton();
         Thread.sleep(5000);
-        System.out.println("/*8.----Enter the Dosages values for 3 row Transfers --*/");
+        System.out.println("/*9.----Enter the Dosages values for 3 row Transfers --*/");
         int k=3;
         while (k <= 7){
             supplyConsolePage.enterBulkTransferDosages(k);
@@ -53,30 +53,30 @@ public class BulkTransfers extends BaseTest {
             Thread.sleep(1000);
             k=k+2;
         }
-        System.out.println("/*9.----select 'To' Automation Supply Location_2  --*/");
+        System.out.println("/*10.----select 'To' Automation Supply Location_2  --*/");
         supplyConsolePage.selectSupplyLocatonTo();
         Thread.sleep(2000);
-        System.out.println("/*10.----click Transfer dialog Modal button --*/");
+        System.out.println("/*11.----click Transfer dialog Modal button --*/");
         supplyConsolePage.clickBulkTransfersModalButton();
         Thread.sleep(2000);
-        System.out.println("/*11.----click Close Modal button --*/");
+        System.out.println("/*12.----click Close Modal button --*/");
         supplyConsolePage.clickBulkTransfersCloseButton();
         Thread.sleep(2000);
-        System.out.println("/*12.----Go to Transactions Tab of Automation Supply Location_1 --*/");
+        System.out.println("/*13.----Go to Transactions Tab of Automation Supply Location_1 --*/");
         supplyConsolePage.clickTransactionsTab();
         Thread.sleep(5000);
-        System.out.println("/*13----Getting id for the latest created Transaction Outgoing 'From' and Incoming 'To'--*/");
-        System.out.println("/*13.1----Get how many Outgoing Transactions 'From' count records --*/");
+        System.out.println("/*14----Getting id for the latest created Transaction Outgoing 'From' and Incoming 'To'--*/");
+        System.out.println("/*14.1----Get how many Outgoing Transactions 'From' count records --*/");
         int countOutgoingTransactions = supplyConsolePage.getRowsOutgoingTransactionsCount();
         Thread.sleep(5000);
         System.out.println("/*---  Outgoing transactions 'from' count:" +countOutgoingTransactions);
         int nn = 1;
         int kk = countOutgoingTransactions;
         // while (nn <= 3 ){
-        System.out.println("/*13.2---Get Outgoing Transaction id 'form' --*/");
+        System.out.println("/*14.2---Get Outgoing Transaction id 'form' --*/");
         String outgoingSupplyTransactionId = supplyConsolePage.getOutgoingSupplyTransactionId(kk);
         System.out.println("/*--outgoing Supply Transaction From id --*/:" +outgoingSupplyTransactionId);
-        System.out.println("/*13.3----Click on the latest created Outgoing Transactions --*/");
+        System.out.println("/*14.3----Click on the latest created Outgoing Transactions --*/");
         supplyConsolePage.clickOnOutgoingTransactions(kk);
         System.out.println("/*--transactions record number --*/:" +kk);
         Thread.sleep(5000);
@@ -101,24 +101,24 @@ public class BulkTransfers extends BaseTest {
         ////nn++;
         ////}
         Thread.sleep(3000);
-        System.out.println("/*14.----Close All Tab's --*/");
+        System.out.println("/*15.----Close All Tab's --*/");
         supplyConsolePage.closeAutomationLocationTab();
         Thread.sleep(3000);
-        System.out.println("/*15.----Go to Supply Locations Tab --*/");
+        System.out.println("/*16.----Go to Supply Locations Tab --*/");
         supplyConsolePage.clickSupplyLocationsTab();
         Thread.sleep(2000);
-        System.out.println("/*16.----Click on Automation Supply Location_2 --*/");
+        System.out.println("/*17.----Click on Automation Supply Location_2 --*/");
         supplyConsolePage.clickOnSupplyLocation_2();
         Thread.sleep(2000);
-        System.out.println("/*17.----Go to Transactions Tab of Automation Supply Location_2 --*/");
+        System.out.println("/*18.----Go to Transactions Tab of Automation Supply Location_2 --*/");
         supplyConsolePage.clickTransactionsTab();
         Thread.sleep(2000);
-        System.out.println("/*18----Get how many Incoming Transactions 'To' count records --*/");
+        System.out.println("/*19----Get how many Incoming Transactions 'To' count records --*/");
         int countIncomingTransactions = supplyConsolePage.getRowsIncomingTransactionsCount();
         Thread.sleep(2000);
         System.out.println("/*---  Incoming transactions 'to' count:" +countIncomingTransactions);
         Thread.sleep(2000);
-        System.out.println("/*19.----Click on Checkboxes Incoming Transactions --*/");
+        System.out.println("/*20.----Click on Checkboxes Incoming Transactions --*/");
         if (countIncomingTransactions >= 3){
             //int j=countIncomingTransactions + 5;
             int j=countIncomingTransactions;
@@ -135,19 +135,19 @@ public class BulkTransfers extends BaseTest {
         }else {
             System.out.println("/*--not all 3 Incoming Transaction records are there--*/");
         }
-        System.out.println("/*20----click Confirm Incoming button Transfer --*/");
+        System.out.println("/*21----click Confirm Incoming button Transfer --*/");
         supplyConsolePage.clickBulkConfirmIncomingTransfersButton();
         Thread.sleep(2000);
-        System.out.println("/*21.----select incoming Supply Distribution for Automation Supply Location_2  --*/");
+        System.out.println("/*22.----select incoming Supply Distribution for Automation Supply Location_2  --*/");
         supplyConsolePage.selectIncomingSupplyDistribution();
         Thread.sleep(2000);
-        System.out.println("/*22.----click on Confirm Incoming Transfer Modal Bulk in the screen --*/");
+        System.out.println("/*23.----click on Confirm Incoming Transfer Modal Bulk in the screen --*/");
         supplyConsolePage.clickOnConfirmModalIncomingTransactionButton();
         Thread.sleep(1000);
-        System.out.println("/*23.--Expecting to see the toast success message - 'You have successfully Confirmed the Transaction' --*/");
+        System.out.println("/*24.--Expecting to see the toast success message - 'You have successfully Confirmed the Transaction' --*/");
         supplyConsolePage.successMessageAppear();
         Thread.sleep(5000); //wait for the popup toast success message disappeared before closing all Tabs
-        System.out.println("/*24----Close Automation_Supply_Location_2 Tab --*/");
+        System.out.println("/*25----Close Automation_Supply_Location_2 Tab --*/");
         supplyConsolePage.closeAutomationLocationTab();
         Thread.sleep(5000);
 
