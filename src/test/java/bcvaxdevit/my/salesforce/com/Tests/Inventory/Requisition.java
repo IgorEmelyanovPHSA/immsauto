94903df27ee6b644374af91425680d6d56c8d1cf
Yulghun <jason.yulghun@phsa.ca>
2022-04-25 14:40:43 -0700

    Requisition on DEVIT

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Requisition.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Requisition.java
@@ -1,90 +1,104 @@
 package bcvaxdevit.my.salesforce.com.Tests.Inventory;
 
 
-import bcvaxdevit.my.salesforce.com.Tests.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import bcvaxdevit.my.salesforce.com.Pages.RequisitionFlowPage;
+import bcvaxdevit.my.salesforce.com.Pages.RequisitionPage;
+import bcvaxdevit.my.salesforce.com.Tests.BaseTest;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Requisition extends BaseTest {
-	
-	@Test
-	public void Can_Create_Requisition_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
-		TestcaseID = "193935"; //C193935
-		RequisitionFlowPage requisitionFlowPage = loginPage.loginAsPPHIS1();
-		System.out.println("/*----1. Login as an PPHIS_bcvaxdevit to Supply Console --*/");
-		Thread.sleep(3000);
-		//requisitionFlowPage.closeTab();
-		System.out.println("/*----2. Close open Tabs --*/");
-		Thread.sleep(3000);
-		requisitionFlowPage.clickDropdownMenu();
-		System.out.println("/*----3. Locate and click on Dropdown Menu --*/");
-		Thread.sleep(3000);
-		requisitionFlowPage.clickSupplyLocationInDropdown();
-		System.out.println("/*----4. Navigate to Supply Locations --*/");
-		Thread.sleep(3000);
-		requisitionFlowPage.clickSupplyLocationName();
-		System.out.println("/*----5. Locate on Automation Supply Location_1 --*/");
-		Thread.sleep(3000);
-		requisitionFlowPage.clickRequestSupplies();
-		System.out.println("/*----6. Navigate to Request Supplies --*/");
-		Thread.sleep(3000);
-		//requisitionFlowPage.clickShipToAddress();
-		Thread.sleep(3000);
-		System.out.println("/*----6. Locate on Automation Supply Location_2 --*/");
-		requisitionFlowPage.inputShipAddress("Atlin Health Centre");
-		//  supplyConsolePagePage.clickOnSupplyLocations();
-		System.out.println("/*----7. Choose Requested Delivery Date --*/");
-		Thread.sleep(3000);
-		requisitionFlowPage.clickInput();
-		Thread.sleep(3000);
-		requisitionFlowPage.clickInput1();
-		Thread.sleep(3000);
-		requisitionFlowPage.inputRequestDate();
-		Thread.sleep(3000);
-		System.out.println("/*----8. Choose Urgency --*/");
-		requisitionFlowPage.clickNextButton();
-		Thread.sleep(3000);
-		System.out.println("/*----9. Add Requisition Line Items --*/");
-		requisitionFlowPage.clickLineItemCheckBox();
-		Thread.sleep(3000);
-		System.out.println("/*----10. Select requested Trades --*/");
-		requisitionFlowPage.clickNextButton();
-		Thread.sleep(3000);
-		System.out.println("/*----11. Input Requested Quantity and Doses --*/");
-		requisitionFlowPage.inputRequestedDose("1");
-		Thread.sleep(3000);
-		System.out.println("/*----12. Save Quantity and Doses --*/");
-		requisitionFlowPage.clickSaveButton();
-		Thread.sleep(3000);
-		System.out.println("/*----13. Submit Requisition --*/");
-		requisitionFlowPage.clickSubmitRequisition();
-		Thread.sleep(5000);
-		System.out.println("/*----14. Confirm and Save Requisition --*/");
-		requisitionFlowPage.clickSaveSubmitRequisition();
-		Thread.sleep(5000);
-		System.out.println("/*----15. Click Edit Expected Delivery Date--*/");
-		requisitionFlowPage.clickEditExpectedDeliveryDate();
-		Thread.sleep(3000);
-		System.out.println("/*----16. Click Calender of Expected Delivery Date--*/");
-		requisitionFlowPage.clickExpectedDeliveryDateCalendar();
-		Thread.sleep(3000);
-		System.out.println("/*----17. Choose The Expected Delivery Date--*/");
-		requisitionFlowPage.inputExpectedDate();
-		Thread.sleep(3000);
-		System.out.println("/*----18. Save Chosen Expected Delivery Date--*/");
-		requisitionFlowPage.clickSaveExpectedDeliveryDate();
-		Thread.sleep(3000);
-		System.out.println("/*----19. Approve Requisition--*/");
-		requisitionFlowPage.clickApproveRequisition();
-		Thread.sleep(5000);
-		System.out.println("/*----20. Select Supply Container With Entering Approved Request Dose--*/");
-		requisitionFlowPage.enterApprovedDose("1");
-		Thread.sleep(9000);
-		//requisitionFlowPage.closeTabs();
-		Assert.assertTrue(false);
 
-		
+	@Test
+	public void Create_Requisition_as_BCVAXDEVIT() throws InterruptedException {
+
+		System.out.println("/*----1. Login as an PPHIS_bcvaxdevit to Supply Console --*/");
+		RequisitionPage requisitionPage = loginPage.loginAsPPHIS1();
+		System.out.println("/*----2. Locate Dropdown Menu --*/");
+		requisitionPage.displaySupplyConsolePage();
+		Thread.sleep(4000);
+		System.out.println("/*----3. Close Other Tabs --*/");
+		requisitionPage.closeTabs();
+		Thread.sleep(4000);
+		requisitionPage.clickDropdownMenu();
+		Thread.sleep(6000);
+		System.out.println("/*----4. Navigate to Supply Locations --*/");
+		requisitionPage.clickSupplyLocationInDropdown();
+		Thread.sleep(6000);
+		System.out.println("/*----5. Locate on Automation Supply Location_1 --*/");
+		requisitionPage.clickSupplyLocationName();
+		Thread.sleep(4000);
+		System.out.println("/*----6. Navigate to Request Supplies --*/");
+		requisitionPage.clickRequestSupplies();
+		Thread.sleep(3000);
+		System.out.println("/*----7. Choose Ship To Address --*/");
+		requisitionPage.inputShipAddress();
+		System.out.println("/*----8. Locate on Automation Supply Location_1 --*/");
+		requisitionPage.LocateAddress("Atlin Health Centre");
+		System.out.println("/*----9. Choose Requested Delivery Date --*/");
+		Thread.sleep(3000);
+		requisitionPage.inputRequestDate();
+		System.out.println("/*----10. Choose Urgency --*/");
+		requisitionPage.clickNextButton();
+		Thread.sleep(3000);
+		System.out.println("/*----11. Add Requisition Line Items --*/");
+		requisitionPage.clickLineItemCheckBox();
+		Thread.sleep(3000);
+		System.out.println("/*----12. Select requested Trades --*/");
+		requisitionPage.clickNextButton();
+		Thread.sleep(3000);
+		System.out.println("/*----13. Input Requested Quantity and Doses --*/");
+		requisitionPage.inputRequestedDose("1");
+		Thread.sleep(3000);
+		System.out.println("/*----14. Save Quantity and Doses --*/");
+		requisitionPage.clickSaveButton();
+		Thread.sleep(3000);
+		System.out.println("/*----15. Submit Requisition --*/");
+		requisitionPage.clickSubmitRequisition();
+		Thread.sleep(5000);
+		System.out.println("/*----16. Confirm and Save Requisition --*/");
+		requisitionPage.clickSaveSubmitRequisition();
+		Thread.sleep(5000);
+		System.out.println("/*----17. Click Edit Expected Delivery Date--*/");
+		requisitionPage.clickEditExpectedDeliveryDate();
+		Thread.sleep(3000);
+		System.out.println("/*----18. Click Calender of Expected Delivery Date--*/");
+		requisitionPage.clickExpectedDeliveryDateCalendar();
+		Thread.sleep(3000);
+		System.out.println("/*----19. Choose The Expected Delivery Date--*/");
+		requisitionPage.inputExpectedDate();
+		Thread.sleep(3000);
+		System.out.println("/*----20. Save Chosen Expected Delivery Date--*/");
+		requisitionPage.clickSaveExpectedDeliveryDate();
+		Thread.sleep(3000);
+		System.out.println("/*----21. Approve Requisition--*/");
+		requisitionPage.clickApproveRequisition();
+		Thread.sleep(4000);
+		System.out.println("/*----22. Select Supply Container With Entering Approved Request Dose--*/");
+		requisitionPage.enterApprovedDose("1");
+		Thread.sleep(5000);
+		System.out.println("/*----23. Save Approved Request Dose--*/");
+		requisitionPage.clickSaveApprovedRequisition();
+		Thread.sleep(4000);
+		System.out.println("/*----24. Ship Requisition--*/");
+		requisitionPage.clickShipRequisition();
+		Thread.sleep(7000);
+
+		String actual ="Ship Requisition";
+		Assert.assertEquals(requisitionPage.ShipRequisition(),actual);
+
+		System.out.println("/*----25. Save Shipping Requisition--*/");
+		requisitionPage.clickSaveShipRequisition();
+		Thread.sleep(3000);
+		System.out.println("/*----26. Receive Requisition--*/");
+		requisitionPage.clickReceiveRequestBtn();
+		Thread.sleep(3000);
+		System.out.println("/*----27. Receive Requisition--*/");
+		requisitionPage.clickSearchSupplyDistributions();
+		Thread.sleep(3000);
+		//requisitionPage.SelectSupplyDistributionTo2("SDST-0000000398");
+
 	}
-	
+
 }
