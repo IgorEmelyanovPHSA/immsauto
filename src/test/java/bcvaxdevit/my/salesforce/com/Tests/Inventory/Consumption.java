743c6ed0b61f1ccd91ef6734b4d979beefecd48f
annesunil <sunil.anne@phsa.ca>
2022-04-29 08:51:29 -0700

    Create Consumption.java

---
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Consumption.java
@@ -0,0 +1,29 @@
+package bcvaxdevit.my.salesforce.com.Tests.Inventory;
+
+import bcvaxdevit.my.salesforce.com.Tests.BaseTest;
+import org.testng.annotations.Test;
+
+public class Consumption extends BaseTest {
+	
+	
+	@Test
+	
+	public void Validate_Consumption_as_an_Clinician_BCVAXDEVIT() throws InterruptedException {
+		TestcaseID = "189594"; //189594
+		System.out.println("/*----1. Login as an Clinician_BCVAXDEVIT to Supply Console --*/");
+		System.out.println("/*----2. Navigate to Health Connect - Supply Console --*/");
+		System.out.println("/*----3. Identify Supply location with some stock --*/");
+		System.out.println("/*----4. Identify matching clinic with the supply location--*/");
+		System.out.println("/*----5. Register a user--*/");
+		System.out.println("/*----6. Click to verify if the user is eligible--*/");
+		System.out.println("/*----7. Go through the vaccination process till vaccine admin--*/");
+		System.out.println("/*----8. Go through the vaccination process--*/");
+		System.out.println("/*----9. Navigate back to Health Connect - Supply Console--*/");
+		System.out.println("/*----10. Get the values for Supply Container, Default Supply Distribution and Supply  Item where you can see Consumed Doses field--*/");
+		
+		
+		
+	}
+}
+
+
