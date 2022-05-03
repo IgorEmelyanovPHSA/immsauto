0c084048db253c7840a00e37efea8df9c611f829
annesunil <sunil.anne@phsa.ca>
2022-05-03 16:05:14 -0700

    WIP

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Consumption.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Consumption.java
@@ -13,42 +13,42 @@ public class Consumption extends BaseTest {
 	@Test(testName = "Inventory Management - Consumption (vaccination)")
 	public void Validate_Consumption_as_an_Clinician_BCVAXDEVIT() throws InterruptedException {
 		TestcaseID = "219969"; //C219969
-		System.out.println("/*----1.Login as an Clinician In-Clinic Experience --*/");
+//		System.out.println("/*----1.Login as an Clinician In-Clinic Experience --*/");
 		InClinicExperiencePage inClinicExperiencePage = loginPage.loginWithClinicianCon();//
-		Thread.sleep(2000);
-		if (inClinicExperiencePage.displaySupplyConsolePage()) {
-			System.out.println("/*----2. User already on Health Connect - Supply Console --*/");
-		} else {
-			System.out.println("/*----2.1. Navigate to Health Connect - Supply Console --*/");
-			inClinicExperiencePage.selectHealthConnectApp();
-			Thread.sleep(2000);
-		}
-		inClinicExperiencePage.closeTabsHCA();
-		System.out.println("/*----3. Close all open tabs --*/");
-		if (inClinicExperiencePage.supplyLocDisplayed()) {
-			System.out.println("/*----4. User is already on Supply loc--*/");
-		} else {
-			System.out.println("/*----4.1. Click Dropdown Menu --*/");
-			inClinicExperiencePage.clickDropdownMenu();
-			Thread.sleep(5000);
-			System.out.println("/*----4.2. Navigate and Select Supply Locations --*/");
-			inClinicExperiencePage.selectSupplyLocationFromDropdown();
-			Thread.sleep(2000);
-		}
-		System.out.println("/*----5. Locate and click Age 12 and Above - Coquitlam - Lincoln Pharmacy & Coquitlam Travel Clinic location --*/");
-		inClinicExperiencePage.selectSupplyLocationName();
-		Thread.sleep(2000);
-		System.out.println("/*----6. Click and navigate to the supply container --> 'Medicago TradeName - MedicagoTestLot001'--*/");
-		inClinicExperiencePage.selectSupplyContainer();
-		Thread.sleep(2000);
-		String remainingDoses = inClinicExperiencePage.getValueOfRemainingDoses();
-		System.out.println("/*----7. remaining doses are: -->" + remainingDoses);
-		Thread.sleep(2000);
-		String remainingQty = inClinicExperiencePage.getValueOfRemainingQty();
-		System.out.println("/*----8. remaining doses are: -->" + remainingQty);
-		Thread.sleep(2000);
-		inClinicExperiencePage.closeTabsHCA();
-		System.out.println("/*----9. Close all open tabs --*/");
+//		Thread.sleep(2000);
+//		if (inClinicExperiencePage.displaySupplyConsolePage()) {
+//			System.out.println("/*----2. User already on Health Connect - Supply Console --*/");
+//		} else {
+//			System.out.println("/*----2.1. Navigate to Health Connect - Supply Console --*/");
+//			inClinicExperiencePage.selectHealthConnectApp();
+//			Thread.sleep(2000);
+//		}
+//		inClinicExperiencePage.closeTabsHCA();
+//		System.out.println("/*----3. Close all open tabs --*/");
+//		if (inClinicExperiencePage.supplyLocDisplayed()) {
+//			System.out.println("/*----4. User is already on Supply loc--*/");
+//		} else {
+//			System.out.println("/*----4.1. Click Dropdown Menu --*/");
+//			inClinicExperiencePage.clickDropdownMenu();
+//			Thread.sleep(5000);
+//			System.out.println("/*----4.2. Navigate and Select Supply Locations --*/");
+//			inClinicExperiencePage.selectSupplyLocationFromDropdown();
+//			Thread.sleep(2000);
+//		}
+//		System.out.println("/*----5. Locate and click Age 12 and Above - Coquitlam - Lincoln Pharmacy & Coquitlam Travel Clinic location --*/");
+//		inClinicExperiencePage.selectSupplyLocationName();
+//		Thread.sleep(2000);
+//		System.out.println("/*----6. Click and navigate to the supply container --> 'Medicago TradeName - MedicagoTestLot001'--*/");
+//		inClinicExperiencePage.selectSupplyContainer();
+//		Thread.sleep(2000);
+//		String remainingDoses = inClinicExperiencePage.getValueOfRemainingDoses();
+//		System.out.println("/*----7. remaining doses are: -->" + remainingDoses);
+//		Thread.sleep(2000);
+//		String remainingQty = inClinicExperiencePage.getValueOfRemainingQty();
+//		System.out.println("/*----8. remaining doses are: -->" + remainingQty);
+//		Thread.sleep(2000);
+//		inClinicExperiencePage.closeTabsHCA();
+//		System.out.println("/*----9. Close all open tabs --*/");
 		Thread.sleep(2000);
 		inClinicExperiencePage.selectICEFromApp();
 		System.out.println("/*----10. Navigate to In Clinic Experience App --*/");
@@ -96,26 +96,29 @@ public class Consumption extends BaseTest {
 		String email1 = "test@qa.com";
 		inClinicExperiencePage.confirmEmail(email1);
 		System.out.println("/*24.'Confirm email address --*/");
-		Thread.sleep(12000);
-		
-		
-		
-		
-		
-		
-		System.out.println("/*----10. Close all open tabs --*/");
-		System.out.println("/*----2. Navigate to Health Connect - Supply Console --*/");
-		System.out.println("/*----3. Identify Supply location with some stock --*/");
-		System.out.println("/*----4. Identify matching clinic with the supply location--*/");
-		System.out.println("/*----5. Register a user--*/");
-		System.out.println("/*----6. Click to verify if the user is eligible--*/");
-		System.out.println("/*----7. Go through the vaccination process till vaccine admin--*/");
-		System.out.println("/*----8. Go through the vaccination process--*/");
-		System.out.println("/*----9. Navigate back to Health Connect - Supply Console--*/");
-		System.out.println("/*----10. Get the values for Supply Container, Default Supply Distribution and Supply  Item where you can see Consumed Doses field--*/");
+		Thread.sleep(2000);
+		inClinicExperiencePage.clickReviewDetails();
+		System.out.println("/*25.Click review details Button--*/");
+		Thread.sleep(2000);
+		inClinicExperiencePage.clickRegisterButtonOnConfirmationPage();
+		System.out.println("/*25.Click register Button on confirmation page--*/");
+		Thread.sleep(5000);
+		inClinicExperiencePage.clickEligibilityButton();
+		System.out.println("/*26.click on check eligibility button on person account and select covid19 option--*/");
+		Thread.sleep(2000);
+//		inClinicExperiencePage.userIsEligibleSuccessMsg();
+//		System.out.println("/*27.User Eligible Message displayed--*/");
+//		Thread.sleep(2000);
+		inClinicExperiencePage.navigateAppointmentSchedulingTab();
+		System.out.println("/*28.Navigate to Appointment Scheduling Tab--*/");
+		Thread.sleep(2000);
+		inClinicExperiencePage.clickToSearchClinic();
+		System.out.println("/*29.Click on search clinic tab --*/");
+		Thread.sleep(2000);
 		
 		
 	}
+	
 }
 
 
