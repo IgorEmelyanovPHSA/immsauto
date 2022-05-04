9688532ac485a2c65617050a1fc3e11be0b5f908
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2022-05-04 12:07:21 -0700

    requisition refactoring

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Requisition.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Requisition.java
@@ -11,8 +11,6 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class Requisition extends BaseTest {
 	
 	@Test
-
-
 	public void Create_Requisition_as_an_PPHIS_BCVAXDEVIT() throws InterruptedException {
 		TestcaseID = "220561"; //C220561
 		System.out.println("/*----1. Login as an PPHIS_BCVAXDEVIT to Supply Console --*/");
