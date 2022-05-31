646a9e242879c3a78ffdfee5f70afb76220ca4e5
Anne <sunil.anne@phsa.ca>
2022-05-31 09:03:48 -0700

    ReceiveSuppliesFlow - Wip

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/ReceiveSupplies.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/ReceiveSupplies.java
@@ -1,4 +1,124 @@
 package bcvaxdevit.my.salesforce.com.Tests.Inventory;
 
-public class ReceiveSupplies {
+import bcvaxdevit.my.salesforce.com.Pages.InClinicExperiencePage;
+import bcvaxdevit.my.salesforce.com.Tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ReceiveSupplies extends BaseTest {
+	@Test()
+	public void Validate_Receive_Supplies_as_an_PPHIS_BCVAXDEVIT() throws InterruptedException {
+		TestcaseID = "111334"; //C111334
+		System.out.println("/*-- 1.Login as an PPHIS user to Supply Location on BCVAXDEVIT --*/");
+		InClinicExperiencePage inClinicExperiencePage = loginPage.loginWithClinicianCon();
+		Thread.sleep(5000);
+		if (inClinicExperiencePage.displaySupplyConsolePage()) {
+			System.out.println("/*-- 2. User already on Health Connect - Supply Console --*/");
+		} else {
+			System.out.println("/*-- 2.1. Navigate to Health Connect - Supply Console --*/");
+			inClinicExperiencePage.selectHealthConnectApp();
+			Thread.sleep(2000);
+		}
+		System.out.println("/*-- 3. Close all open tabs --*/");
+		inClinicExperiencePage.closeTabsHCA();
+		Thread.sleep(2000);
+		System.out.println("/*-- 4. Click Dropdown Menu --*/");
+		inClinicExperiencePage.clickDropdownMenu();
+		Thread.sleep(5000);
+		System.out.println("/*-- 5. Select Supply Items Option from the Drop Down --*/");
+		inClinicExperiencePage.selectSupplyItemsFromDropdown();
+		Thread.sleep(2000);
+		System.out.println("/*-- 6. Click on 'AstraZeneca ChAdOx1-S - ABX3120' Supply Item--*/");
+		inClinicExperiencePage.selectSupplyItemName();
+		Thread.sleep(2000);
+		String doseConversionFactor = inClinicExperiencePage.getDoseConversionFactor();
+		System.out.println("/*-- 7. remaining doses are: -->" + doseConversionFactor);
+		Thread.sleep(2000);
+		System.out.println("/*-- 8. Close all open tabs --*/");
+		inClinicExperiencePage.closeTabsHCA();
+		Thread.sleep(2000);
+		System.out.println("/*-- 9. Click Dropdown Menu --*/");
+		inClinicExperiencePage.clickDropdownMenu();
+		Thread.sleep(5000);
+		System.out.println("/*-- 10. Navigate and Select Supply Locations --*/");
+		inClinicExperiencePage.selectSupplyLocationFromDropdown();
+		Thread.sleep(2000);
+		System.out.println("/*-- 11. Locate and click Age 12 and Above - Coquitlam - Lincoln Pharmacy & Coquitlam Travel Clinic location --*/");
+		inClinicExperiencePage.selectSupplyLocationName();
+		Thread.sleep(2000);
+		System.out.println("/*-- 12. Click and select supply distribution SDST-0000001558 --*/");
+		inClinicExperiencePage.clickSupplyDistribution();
+		Thread.sleep(2000);
+		String supplyName = inClinicExperiencePage.getSupplyDistributionName();
+		System.out.println("/*-- 13. Supply Destribution Name is: -->" + supplyName);
+		Thread.sleep(2000);
+		String supplyDesc = inClinicExperiencePage.getSupplyDistributionDescription();
+		System.out.println("/*-- 14. Supply Destribution Description is: -->" + supplyDesc);
+		Thread.sleep(2000);
+		System.out.println("/*-- 15. Close all open tabs --*/");
+		inClinicExperiencePage.closeTabsHCA();
+		Thread.sleep(2000);
+		if (inClinicExperiencePage.supplyLocDisplayed()) {
+			System.out.println("/*-- 16. User is already on Supply loc--*/");
+		} else {
+			System.out.println("/*-- 16.1. Click Dropdown Menu --*/");
+			inClinicExperiencePage.clickDropdownMenu();
+			Thread.sleep(5000);
+			System.out.println("/*-- 16.2. Navigate and Select Supply Locations --*/");
+			inClinicExperiencePage.selectSupplyLocationFromDropdown();
+			Thread.sleep(2000);
+		}
+		System.out.println("/*-- 17. Locate and click Age 12 and Above - Coquitlam - Lincoln Pharmacy & Coquitlam Travel Clinic location --*/");
+		inClinicExperiencePage.selectSupplyLocationName();
+		Thread.sleep(2000);
+		System.out.println("/*-- 18. Navigate and Select Dropdown to Receive Supplies Button --*/");
+		inClinicExperiencePage.SelectDropDownToClickReceiveSuppliesButton();
+		Thread.sleep(2000);
+		System.out.println("/*-- 19. Click to Receive Supplies Button --*/");
+		inClinicExperiencePage.ClickDropDownToClickReceiveSuppliesButton();
+		Thread.sleep(2000);
+		System.out.println("/*-- 20. Validate Supply Item Filed Present on Layout --*/");
+		String supplyItem = inClinicExperiencePage.validateSupplyItemField();
+		String expectedSupplyItemLabel = "*Supply Item";
+		System.out.println("/*-- 20.1.: -->" + supplyItem + "Field is Present on Layout and is Mandatory");
+		Assert.assertEquals((supplyItem), (expectedSupplyItemLabel));
+		Thread.sleep(2000);
+		System.out.println("/*-- 21. Validate Quantity Filed Present on Layout --*/");
+		String qty = inClinicExperiencePage.validateQTYField();
+		String expectedqtyLabel = "Quantity";
+		System.out.println("/*-- 21.1.: -->" + qty + "Field is Present on Layout and is Mandatory");
+		Assert.assertEquals((qty), (expectedqtyLabel));
+		Thread.sleep(2000);
+		System.out.println("/*-- 22. Validate Dose Conversion Factor Filed Present on Layout --*/");
+		String dcf = inClinicExperiencePage.validateDCFField();
+		String expecteddcfLabel = "Dose Conversion Factor";
+		System.out.println("/*-- 22.1.: -->" + dcf + "Field is Present on Layout");
+		Assert.assertEquals((dcf), (expecteddcfLabel));
+		Thread.sleep(2000);
+		System.out.println("/*-- 23. Validate Doses Filed Present on Layout --*/");
+		String doses = inClinicExperiencePage.validateDosesField();
+		String expecteddosesLabel = "*Doses";
+		System.out.println("/*-- 23.1.: -->" + doses + "Field is Present on Layout");
+		Assert.assertEquals((doses), (expecteddosesLabel));
+		Thread.sleep(2000);
+		System.out.println("/*-- 24. Validate Supply Distribution To Filed Present on Layout --*/");
+		String supplydistributionto = inClinicExperiencePage.validateSupplyDistributionToField();
+		String expectedsupplydistributiontoLabel = "*Supply Distribution To";
+		System.out.println("/*-- 24.1.: -->" + supplydistributionto + "Field is Present on Layout");
+		Assert.assertEquals((supplydistributionto), (expectedsupplydistributiontoLabel));
+		Thread.sleep(2000);
+		System.out.println("/*-- 25. Save Button is Present on Layout --*/");
+		inClinicExperiencePage.ValidateSaveButtonIsDisplayedOnReceiveSupplies();
+		Thread.sleep(2000);
+		System.out.println("/*-- 26. Cancel Button is Present on Layout --*/");
+		inClinicExperiencePage.ValidateCancelButtonIsDisplayedOnReceiveSupplies();
+		Thread.sleep(2000);
+		System.out.println("/*-- 27. Cancel Button is Present on Layout --*/");
+		inClinicExperiencePage.clickSupplyItemTextBox();
+		Thread.sleep(2000);
+//		System.out.println("/*-- 28. Select AstraZeneca ChAdOx1-S - ABX3120 Supply Item --*/");
+//		inClinicExperiencePage.selectSupplyItem("AstraZeneca ChAdOx1-S - ABX3120");
+//		Thread.sleep(20000);
+	}
+	
 }
