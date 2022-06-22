24f05662dda6b853f729edd4e69d8952be6a917a
igor.emelyanov <igor.emelyanov@phsa.ca>
2022-06-22 12:18:34 -0700

    gitignore targets

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Transfer.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Transfer.java
@@ -16,48 +16,48 @@ public class Transfer extends BaseTest {
     @Test
     public void Can_do_Transfer_by_Dosages_from_one_Clinic_to_Another_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
         TestcaseID = "220550"; //C220550
-        System.out.println("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
+        log("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
         SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHIS();
         Thread.sleep(5000);
         //Assert.assertTrue(false);
-        System.out.println("/*2.----Supply Console Page displayed --*/");
+        log("/*2.----Supply Console Page displayed --*/");
         supplyConsolePage.verifyIsSupplyPageDisplayed();
         Thread.sleep(5000);
-        System.out.println("/*3.----Close All previously opened Tab's --*/");
+        log("/*3.----Close All previously opened Tab's --*/");
         supplyConsolePage.closeAutomationLocationTab();
         Thread.sleep(2000);
-        System.out.println("/*4.----Go to Supply Locations Tab --*/");
+        log("/*4.----Go to Supply Locations Tab --*/");
         supplyConsolePage.clickSupplyLocationsTab();
         ////// Supply Location_1 -> Outcoming
-        System.out.println("/*5.----Click on Automation Supply Location_1 --*/");
+        log("/*5.----Click on Automation Supply Location_1 --*/");
         supplyConsolePage.clickOnSupplyLocation_1();
         Thread.sleep(5000);
-        //System.out.println("/*-- 6. Click and navigate to the supply container --> 'Pfizer mRNA BNT162b2 - EK4241' --*/");
+        //log("/*-- 6. Click and navigate to the supply container --> 'Pfizer mRNA BNT162b2 - EK4241' --*/");
         //supplyConsolePage.selectSupplyContainer();
         Thread.sleep(2000);
-        System.out.println("/*6.----Quantity Remaining Doses/Remaining Quantity check Before --*/");
+        log("/*6.----Quantity Remaining Doses/Remaining Quantity check Before --*/");
         double remainingDoses_before = supplyConsolePage.getValueOfRemainingDoses();
-        System.out.println("/*-- . remaining doses are: -->" + remainingDoses_before);
+        log("/*-- . remaining doses are: -->" + remainingDoses_before);
         Thread.sleep(2000);
         double remainingQty_before = supplyConsolePage.getValueOfRemainingQty();
-        System.out.println("/*-- . remaining Quantity are: -->" + remainingQty_before);
+        log("/*-- . remaining Quantity are: -->" + remainingQty_before);
         Thread.sleep(2000);
-        System.out.println("/*7.----Click on Container's dropdown --*/");
+        log("/*7.----Click on Container's dropdown --*/");
         supplyConsolePage.clickOnContainerDropDownMenu();
         Thread.sleep(2000);
-        System.out.println("/*8.----select Transfer from the DropDownMenu dropdown menu --*/");
+        log("/*8.----select Transfer from the DropDownMenu dropdown menu --*/");
         supplyConsolePage.selectTransferFromDropDown();
         Thread.sleep(2000);
-        System.out.println("/*9.----Picked up the Trade Vaccine Name --*/");
+        log("/*9.----Picked up the Trade Vaccine Name --*/");
         String tradeName = supplyConsolePage.getVaccineName();//Pfizer mRNA BNT162b2 - EK4241
-        System.out.println("/*--  the Trade Name is:  " + tradeName);
+        log("/*--  the Trade Name is:  " + tradeName);
         Thread.sleep(2000);
-        System.out.println("/*10.----Picked up the Dose Conversation Factor --*/");
+        log("/*10.----Picked up the Dose Conversation Factor --*/");
         //Double dose_conversation_factor = 5.0;
         double dose_conversation_factor = supplyConsolePage.getDoseConversationFactor();
-        System.out.println("/*--  the Dose Conversation Factor is:  " + dose_conversation_factor);
+        log("/*--  the Dose Conversation Factor is:  " + dose_conversation_factor);
         Thread.sleep(2000);
-        System.out.println("/*10.----Entering 10 Doses in the Container-Transfer Form --*/");
+        log("/*10.----Entering 10 Doses in the Container-Transfer Form --*/");
         supplyConsolePage.enterTransferDosages("10");
         System.out.println("/*11.----select 'To' Automation Supply Location_2  --*/");
         supplyConsolePage.selectSupplyLocation_2_To();;
