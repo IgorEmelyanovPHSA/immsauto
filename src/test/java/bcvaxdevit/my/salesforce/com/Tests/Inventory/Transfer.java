a108cdfe61f7ecc77f5108c1e80c7d07c036cb16
igor.emelyanov <igor.emelyanov@phsa.ca>
2022-07-14 16:09:41 -0700

    updates for Transfer within the Same Clinic

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Transfer.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/Transfer.java
@@ -12,7 +12,7 @@ import static org.testng.Assert.assertEquals;
 @Listeners({TestListener.class})
 public class Transfer extends BaseTest {
 	
-	@Test
+	@Test(priority = 1)
 	public void Can_do_Transfer_by_Dosages_from_one_Clinic_to_Another_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
 		TestcaseID = "220550"; //C220550
 		log("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
@@ -153,7 +153,7 @@ public class Transfer extends BaseTest {
 		
 	}
 
-	@Test
+	@Test(priority = 2)
 	public void Can_do_Transfer_by_Quantity_from_one_Clinic_to_Another_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
 		TestcaseID = "220550"; //C220550
 		log("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
@@ -197,7 +197,7 @@ public class Transfer extends BaseTest {
 		double dose_conversation_factor = supplyConsolePage.getDoseConversationFactor();
 		log("/*--  the Dose Conversation Factor is:  " + dose_conversation_factor);
 		Thread.sleep(2000);
-		log("/*10.----Entering 10 Doses in the Container-Transfer Form --*/");
+		log("/*10.----Entering 10 Quantity in the Container-Transfer Form --*/");
 		supplyConsolePage.enterTransferQuantity("10");
 		System.out.println("/*11.----select 'To' Automation Supply Location_2  --*/");
 		supplyConsolePage.selectSupplyLocation_2_To();
@@ -294,7 +294,7 @@ public class Transfer extends BaseTest {
 
 	}
 
-	@Test
+	@Test(priority = 3)
 	public void Can_do_Transfer_by_Dosages_within_the_same_Clinic_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
 		TestcaseID = "220557";
 		System.out.println("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
@@ -387,5 +387,101 @@ public class Transfer extends BaseTest {
 		Thread.sleep(2000);
 		
 	}
+
+	@Test(priority = 4)
+	public void Can_do_Transfer_by_Quantity_within_the_same_Clinic_as_PPHIS_BCVAXDEVIT() throws InterruptedException {
+		TestcaseID = "220557";
+		System.out.println("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
+		SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHIS();
+		Thread.sleep(5000);
+		//Assert.assertTrue(false);
+		System.out.println("/*2.----Supply Console Page displayed --*/");
+		supplyConsolePage.verifyIsSupplyPageDisplayed();
+		Thread.sleep(5000);
+		System.out.println("/*3.----Close All previously opened Tab's --*/");
+		supplyConsolePage.closeAutomationLocationTab();
+		Thread.sleep(2000);
+		System.out.println("/*4.----Go to Supply Locations Tab --*/");
+		supplyConsolePage.clickSupplyLocationsTab();
+		System.out.println("/*5.----Click on Automation Supply Location_1 --*/");
+		supplyConsolePage.clickOnSupplyLocation_1();
+		Thread.sleep(5000);
+		//System.out.println("/*-- 6. Click and navigate to the supply container --> 'Pfizer mRNA BNT162b2 - EK4241' --*/");
+		//supplyConsolePage.selectSupplyContainer();
+		Thread.sleep(2000);
+		/////////////////////Doses and Quantity BEFORE//////////////////////////////////
+		System.out.println("/*6.----Quantity Remaining Doses/Remaining Quantity check Before for Distribution_1_1 --*/");
+		double remainingDoses_before_Distribution_1_1 = supplyConsolePage.getValueOfRemainingDoses();
+		System.out.println("/*-- . Distribution_1_1 remaining doses Before are: -->" + remainingDoses_before_Distribution_1_1);
+		Thread.sleep(2000);
+		double remainingQty_before_Distribution_1_1 = supplyConsolePage.getValueOfRemainingQty();
+		System.out.println("/*-- . Distribution_1_1 remaining Quantity Before are: -->" + remainingQty_before_Distribution_1_1);
+		Thread.sleep(2000);
+		log("/*7.----Quantity Remaining Doses/Remaining Quantity check Before for Distribution_1_2 --*/");
+		double remainingDoses_before_Distribution_1_2 = supplyConsolePage.getValueOfRemainingDosesDistribution_1_2();
+		System.out.println("/*-- . Distribution_1_2 remaining doses Before are: -->" + remainingDoses_before_Distribution_1_2);
+		Thread.sleep(2000);
+		double remainingQty_before_Distribution_1_2 = supplyConsolePage.getValueOfRemainingQtyDistribution_1_2();
+		System.out.println("/*-- . Distribution_1_2 remaining Quantity Before are: -->" + remainingQty_before_Distribution_1_2);
+		Thread.sleep(2000);
+		/////////Do Transfer from Distribution_1_1 to Distribution_1_2/////////
+		System.out.println("/*8.----Click on Container's dropdown --*/");
+		supplyConsolePage.clickOnContainerDropDownMenu();
+		Thread.sleep(2000);
+		System.out.println("/*9.----select Transfer from the DropDownMenu dropdown menu --*/");
+		supplyConsolePage.selectTransferFromDropDown();
+		Thread.sleep(2000);
+		System.out.println("/*10.----Picked up the Trade Vaccine Name --*/");
+		String tradeName = supplyConsolePage.getVaccineName();//Pfizer mRNA BNT162b2 - EK4241
+		System.out.println("/*--  the Trade Name is:  " + tradeName);
+		Thread.sleep(2000);
+		System.out.println("/*11.----Picked up the Dose Conversation Factor --*/");
+		//Double dose_conversation_factor = 5.0;
+		double dose_conversation_factor = supplyConsolePage.getDoseConversationFactor();
+		System.out.println("/*--  the Dose Conversation Factor is:  " + dose_conversation_factor);
+		Thread.sleep(2000);
+		System.out.println("/*12.----Entering 10 Quantity in the Container-Transfer Form --*/");
+		supplyConsolePage.enterTransferQuantity("10");
+		System.out.println("/*13.----select 'To' 'Automation Supply Location_1'  --*/");
+		supplyConsolePage.selectSupplyLocation_1_To();
+		;
+		Thread.sleep(2000);
+		System.out.println("/*14.----select 'Supply Distribution_1_2' 'To'  --*/");
+		supplyConsolePage.selectSameClinicSupplyDistribution();
+		Thread.sleep(2000);
+		System.out.println("/*15.----click Transfer dialog Modal button --*/");
+		supplyConsolePage.clickBulkTransfersModalButton();
+		Thread.sleep(2000);
+		System.out.println("/*16.----click Close Modal button --*/");
+		supplyConsolePage.clickBulkTransfersCloseButton();
+		Thread.sleep(5000);
+		/////////////////////Doses and Quantity AFTER///////////////////////////////////
+		System.out.println("/*17----Quantity Remaining Doses/Remaining Quantity check After for Distribution_1_1 --*/");
+		double remainingDoses_after_Distribution_1_1 = supplyConsolePage.getValueOfRemainingDoses();
+		System.out.println("/*-- . remaining doses Distribution_1_1 After are: -->" + remainingDoses_after_Distribution_1_1);
+		Thread.sleep(2000);
+		double remainingQty_after_Distribution_1_1 = supplyConsolePage.getValueOfRemainingQty();
+		System.out.println("/*-- . remaining Quantity  Distribution_1_1 After are: -->" + remainingQty_after_Distribution_1_1);
+		Thread.sleep(2000);
+		System.out.println("/*18----Quantity Remaining Doses/Remaining Quantity check After for Distribution_1_2 --*/");
+		double remainingDoses_after_Distribution_1_2 = supplyConsolePage.getValueOfRemainingDosesDistribution_1_2();
+		System.out.println("/*-- . remaining doses  Distribution_1_2 After are: -->" + remainingDoses_after_Distribution_1_2);
+		Thread.sleep(2000);
+		double remainingQty_after_Distribution_1_2 = supplyConsolePage.getValueOfRemainingQtyDistribution_1_2();
+		System.out.println("/*-- . remaining Quantity  Distribution_1_2 After are: -->" + remainingQty_after_Distribution_1_2);
+		Thread.sleep(2000);
+		//////////Validation for Distribution_1_1(From) and Distribution_1_2(To)
+		System.out.println("/*19.----Validate Remaining Doses and Remaining Quantities values for Distribution_1_1 --*/");
+		assertEquals((remainingDoses_before_Distribution_1_1 - 50), remainingDoses_after_Distribution_1_1);
+		assertEquals(((remainingDoses_before_Distribution_1_1 - 50) / dose_conversation_factor), remainingQty_after_Distribution_1_1);
+		Thread.sleep(2000);
+		System.out.println("/*20.----Validate Remaining Doses and Remaining Quantities values for Distribution_1_2 --*/");
+		assertEquals((remainingDoses_before_Distribution_1_2 + 50), remainingDoses_after_Distribution_1_2);
+		assertEquals(((remainingDoses_before_Distribution_1_2 + 50) / dose_conversation_factor), remainingQty_after_Distribution_1_2);
+		Thread.sleep(2000);
+
+	}
+
+
 
 }
