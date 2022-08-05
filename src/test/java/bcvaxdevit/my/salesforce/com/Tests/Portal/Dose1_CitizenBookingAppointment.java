89f99f2b3e2d199ed9425f7bfbe65a2506e459b3
DmytroBilousPhsa <87721889+DmytroBilousPhsa@users.noreply.github.com>
2022-08-05 09:03:36 -0700

    Update Dose1_CitizenBookingAppointment.java

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Portal/Dose1_CitizenBookingAppointment.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Portal/Dose1_CitizenBookingAppointment.java
@@ -171,7 +171,7 @@ public class Dose1_CitizenBookingAppointment extends BaseTest {
 		log("/*16---Verify appointment conformation message is displayed--*/");
 		bookAnAppointmentPage.appointmentConfirmationPageDisplayed();
 		}
-
+	}
 		/*
 		Go through the citizen flow as someone who does not have a PHN number
 		Go through the citizen flow as someone who has a PHN number
@@ -180,5 +180,3 @@ public class Dose1_CitizenBookingAppointment extends BaseTest {
 		Go through the citizen flow where Booking capacity is one less than the capacity and two citizen booking at almost the same time
 		Go through the citizen flow where Booking capacity is full but one citizen cancelled so it became available for booking, then it gets booked and meets the capacity and should not be available anymore
 		*/
-
-	}
