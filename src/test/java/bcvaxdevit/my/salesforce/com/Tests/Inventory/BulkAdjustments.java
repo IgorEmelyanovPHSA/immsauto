c98fb01210d0ec53275087718f4c0fec6718ce64
DmytroB <dmytro.bilous@phsa.ca>
2022-09-23 13:01:29 -0700

    add testcase bulk adjustment by quantities and some refactoring for consolePage

--- src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/BulkAdjustments.java
+++ src/test/java/bcvaxdevit/my/salesforce/com/Tests/Inventory/BulkAdjustments.java
@@ -1,6 +1,7 @@
 package bcvaxdevit.my.salesforce.com.Tests.Inventory;
 
 import Utilities.TestListener;
+import bcvaxdevit.my.salesforce.com.Pages.CommonMethods;
 import bcvaxdevit.my.salesforce.com.Pages.SupplyConsolePage;
 import bcvaxdevit.my.salesforce.com.Pages.Utils;
 import bcvaxdevit.my.salesforce.com.Tests.BaseTest;
@@ -19,30 +20,36 @@ import static org.testng.Assert.assertTrue;
 
 @Listeners({TestListener.class})
 public class BulkAdjustments extends BaseTest {
-	
-	
-	@DataProvider(name = "value")
+
+	@DataProvider(name = "dosesAmount")
 	public static Object[][] primeNumbers() {
 		return new Object[][]{{"50"}, {"-30"}};
 	}
+
+	@DataProvider(name = "quantitiesAmount")
+	public static Object[][] primeNumbers2() {
+		return new Object[][]{{"3"},{"-2"}};
+	}
 	
-	@Test(dataProvider = "value")
-	public void Can_Do_Bulk_Adjustment_ByDosages_Positive_And_Negative_Value_AS_PPHIS_BCVAXDEVIT(String value) throws Exception {
+	@Test(dataProvider = "dosesAmount")
+	public void Can_Do_Bulk_Adjustment_ByDosages_Positive_And_Negative_Value_AS_PPHIS(String value) throws Exception {
 		TestcaseID = "223360"; //C223360
 		log("Target Environment: "+ Utils.getTargetEnvironment());
 		AllureLifecycle lifecycle = Allure.getLifecycle();
 		double amountOfDosesToAdjust = Double.parseDouble(value);
 		boolean isNegativeFlag = isNegative(amountOfDosesToAdjust);
+		String reasonForAdjustment = "Administered Vaccine";
+
 		if (isNegativeFlag == false) {
-			log("/*0.----Positive Scenario: Can_Do_Bulk_Adjustment_ByDosages_Positive_Value_AS_PPHIS_BCVAXDEVIT--*/");
-			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByDosages_Positive_Value_AS_PPHIS_BCVAXDEVIT"));
+			log("/*0.----Positive Scenario: Can_Do_Bulk_Adjustment_ByDosages_Positive_Value_AS_PPHIS--*/");
+			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByDosages_Positive_Value_AS_PPHIS"));
 		} else {
-			log("/*0.----Negative Scenario: Can_Do_Bulk_Adjustment_ByDosages_Negative_Value_AS_PPHIS_BCVAXDEVIT--*/");
-			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByDosages_Negative_Value_AS_PPHIS_BCVAXDEVIT"));
+			log("/*0.----Negative Scenario: Can_Do_Bulk_Adjustment_ByDosages_Negative_Value_AS_PPHIS--*/");
+			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByDosages_Negative_Value_AS_PPHIS"));
 		}
 		log("/*----Amount Adjustment Doses " + amountOfDosesToAdjust + " --*/");
 		
-		log("/*1.----Login as an PPHIS_bcvaxdevit to Supply Console --*/");
+		log("/*1.----Login as an PPHIS to Supply Console --*/");
 		SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHISWithParameters();
 		Thread.sleep(5000);
 		
@@ -78,7 +85,7 @@ public class BulkAdjustments extends BaseTest {
 			log("/*--not enough records for Bulk actions--*/");
 		}
 		int numberOfRows = 3;  //Default COUNT limited to 3 rows as per step7
-		//Remaining Doses and Quantity count // 3 rows, ref BulkWastage step7 containers count
+		//Remaining Doses and Quantity count // 3 rows, ref step7 containers count
 		log("/*8.----Read Remaining Doses And Quantity Before Deduction --*/");
 		HashMap<Integer, ArrayList<Double>> remainingDosesAndQuantityBeforeAdjustment = supplyConsolePage.countDosesAndQuantityMap(numberOfRows);
 		
@@ -86,8 +93,8 @@ public class BulkAdjustments extends BaseTest {
 		supplyConsolePage.clickBulkAdjustmentButton();
 		Thread.sleep(5000);
 		
-		log("/*10.----Enter the Dosages values for 3 row and reason for wastage --*/");
-		supplyConsolePage.enterBulkAdjustmentByDosageWithReasonForAdjustment(amountOfDosesToAdjust, numberOfRows);
+		log("/*10.----Enter the Dosages values for 3 row and reason for Adjustment: " +reasonForAdjustment +"--*/");
+		supplyConsolePage.enterBulkByDosageWithReason(amountOfDosesToAdjust,reasonForAdjustment, numberOfRows);
 		
 		log("/*11.----Click button Adjustment on Container - Adjustment page --*/");
 		supplyConsolePage.clickAdjustmentButtonContainerAdjustmentPage();
@@ -129,15 +136,118 @@ public class BulkAdjustments extends BaseTest {
 			//Comparing results
 			log("Compering remaining doses after adjustment " + remainingDosesAfterAdjustment + " vs calculated doses after adjustment " + calculatedDosesAfterAdjustment);
 			assertEquals(remainingDosesAfterAdjustment, calculatedDosesAfterAdjustment);
-			//assertTrue(Double.compare(remainingDosesAfterAdjustment, calculatedDosesAfterAdjustment) == 0, "Values are different!");
-			
+
 			log("Compering remaining quantity after adjustment " + remainingQuantityAfterAdjustment + " vs calculated quantity after adjustment " + calculatedRemainingQuantityAfterAdjustment);
 			assertEquals(remainingQuantityAfterAdjustment, calculatedRemainingQuantityAfterAdjustment);
-			//assertTrue(Double.compare(remainingQuantityAfterAdjustment, calculatedRemainingQuantityAfterAdjustment) == 0, "Values are different!");
 
 			log("Compering dose conversion factor before adjustment " + doseConversionFactorBeforeAdjustment + " vs dose conversion factor after adjustment " + doseConversionAfterAdjustment);
 			assertEquals(doseConversionFactorBeforeAdjustment, doseConversionAfterAdjustment);
-			//assertTrue(Double.compare(doseConversionFactorBeforeAdjustment, doseConversionAfterAdjustment) == 0, "Values are different!");
+			}
+	}
+
+	@Test(dataProvider = "quantitiesAmount")
+	public void Can_Do_Bulk_Adjustment_ByQuantities_Positive_And_Negative_Value_AS_PPHIS(String quantity) throws Exception {
+		TestcaseID = "223360"; //C223360
+		log("Target Environment: "+ Utils.getTargetEnvironment());
+		AllureLifecycle lifecycle = Allure.getLifecycle();
+		double amountOfQuantityToAdjust = Double.parseDouble(quantity);
+		boolean isNegativeFlag = isNegative(amountOfQuantityToAdjust);
+		String reasonForAdjustment = "Administered Vaccine";
+
+		if (isNegativeFlag == false) {
+			log("/*0.----Positive Scenario: Can_Do_Bulk_Adjustment_ByQuantities_Positive_Value_AS_PPHIS--*/");
+			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByQuantities_Positive_Value_AS_PPHIS"));
+		} else {
+			log("/*0.----Negative Scenario: Can_Do_Bulk_Adjustment_ByQuantities_Negative_Value_AS_PPHIS--*/");
+			lifecycle.updateTestCase(testResult -> testResult.setName("Can_Do_Bulk_Adjustment_ByQuantities_Negative_Value_AS_PPHIS"));
+		}
+		log("/*----Amount Adjustment Quantities " + amountOfQuantityToAdjust + " --*/");
+
+		log("/*1.----Login as an PPHIS to Supply Console --*/");
+		SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHISWithParameters();
+		Thread.sleep(5000);
+
+		log("/*2.----Validate if Supply Console Page displayed --*/");
+		CommonMethods common = new CommonMethods(getDriver());
+		common.goToSupplyPageIfNeededAndConfirmPageIsDisplayed();
+
+		log("/*3.----Click on Automation Supply Location_1 --*/");
+		supplyConsolePage.clickOnSupplyLocation_1();
+		Thread.sleep(5000);
+
+		log("/*4.----Get Supply Containers count outcoming records --*/");
+		int countSupplyContainers = supplyConsolePage.getRowsSupplyContainersFromCount();
+		log("/*---     count:" + countSupplyContainers);
+
+		log("/*5.----Click on Container's records Checkboxes --*/");
+		if (countSupplyContainers >= 3) {
+			int k = 1;
+			while (k <= 3) {
+				supplyConsolePage.clickOnSupplyContainerCheckbox(k);
+				log("/*---     containers record number: " + k);
+				Thread.sleep(1000);
+				k++;
+			}
+		} else {
+			log("/*--not enough records for Bulk actions--*/");
+		}
+		int numberOfRows = 3;  //Default COUNT limited to 3 rows as per step5
+		//Remaining Doses and Quantity count // 3 rows, ref step5 containers count
+		log("/*6.----Read Remaining Doses And Quantity Before Deduction --*/");
+		HashMap<Integer, ArrayList<Double>> remainingDosesAndQuantityBeforeAdjustment = supplyConsolePage.countDosesAndQuantityMap(numberOfRows);
+
+		log("/*7.----Click on bulk Adjustment button on Supply page--*/");
+		supplyConsolePage.clickBulkAdjustmentButton();
+		Thread.sleep(5000);
+
+		log("/*8.----Enter the Quantities values for 3 rows and reason for adjustment: " +reasonForAdjustment +"--*/");
+		supplyConsolePage.enterBulkByQuantitiesWithReason(amountOfQuantityToAdjust, reasonForAdjustment, numberOfRows);
+
+		log("/*9.----Click button Adjustment on Container - Adjustment page --*/");
+		supplyConsolePage.clickAdjustmentButtonContainerAdjustmentPage();
+		Thread.sleep(3000);
+
+		log("/*10.----Read Remaining Doses And Quantity After Adjustment --*/");
+		HashMap<Integer, ArrayList<Double>> actualRemainingDosesAndQuantityAfterAdjustment = supplyConsolePage.countDosesAndQuantityMap(numberOfRows);
+
+		log("/*11.----Calculating Remaining Doses And Quantity After Adjustment --*/");
+		HashMap<Integer, ArrayList<Double>> calculatedRemainingDosesAndQuantityAfterAdjustment = new HashMap<>();
+		for (int i = 0; i < remainingDosesAndQuantityBeforeAdjustment.size(); i++) {
+			ArrayList<Double> writeToList = new ArrayList<>();
+			ArrayList<Double> readFromList = remainingDosesAndQuantityBeforeAdjustment.get(i);
+			double remainingDoses = readFromList.get(0);
+			double remainingQuantity = readFromList.get(1);
+			double doseConversionFactor = readFromList.get(2);
+			//Actual calculation
+			double afterAdjustmentDoses =  remainingDoses + (amountOfQuantityToAdjust * doseConversionFactor);
+			double afterAdjustmentQuantity = remainingQuantity + amountOfQuantityToAdjust;
+			writeToList.add(afterAdjustmentDoses);
+			writeToList.add(afterAdjustmentQuantity);
+			writeToList.add(doseConversionFactor);
+			calculatedRemainingDosesAndQuantityAfterAdjustment.put(i, writeToList);
+		}
+
+		log("/*12.----Compering Remaining Doses and Quantity actual vs calculated--*/");
+		//Comparing 2 objects actualRemainingDosesAndQuantityAfterAdjustment vs calculatedRemainingDosesAndQuantityAfterAdjustment
+		for (int i = 0; i < actualRemainingDosesAndQuantityAfterAdjustment.size(); i++) {
+			ArrayList<Double> afterDeduction = actualRemainingDosesAndQuantityAfterAdjustment.get(i);
+			double remainingDosesAfterAdjustment = afterDeduction.get(0);
+			double remainingQuantityAfterAdjustment = afterDeduction.get(1);
+			double doseConversionFactorBeforeAdjustment = afterDeduction.get(2);
+			ArrayList<Double> calculated = calculatedRemainingDosesAndQuantityAfterAdjustment.get(i);
+			double calculatedDosesAfterAdjustment = calculated.get(0);
+			double calculatedRemainingQuantityAfterAdjustment = calculated.get(1);
+			double doseConversionAfterAdjustment = calculated.get(2);
+
+			//Comparing results
+			log("Compering remaining doses after adjustment " + remainingDosesAfterAdjustment + " vs calculated doses after adjustment " + calculatedDosesAfterAdjustment);
+			assertEquals(remainingDosesAfterAdjustment, calculatedDosesAfterAdjustment);
+
+			log("Compering remaining quantity after adjustment " + remainingQuantityAfterAdjustment + " vs calculated quantity after adjustment " + calculatedRemainingQuantityAfterAdjustment);
+			assertEquals(remainingQuantityAfterAdjustment, calculatedRemainingQuantityAfterAdjustment);
+
+			log("Compering dose conversion factor before adjustment " + doseConversionFactorBeforeAdjustment + " vs dose conversion factor after adjustment " + doseConversionAfterAdjustment);
+			assertEquals(doseConversionFactorBeforeAdjustment, doseConversionAfterAdjustment);
 		}
 	}
 	
