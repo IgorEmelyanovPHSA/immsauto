0356e21cf8135022b64c85fc3f1eae00a4b6b2dd
DmytroB <dmytro.bilous@phsa.ca>
2022-12-01 12:50:15 -0800

    refactoring

---
+++ src/test/java/bcvax/tests/Inventory/Wastage.java
@@ -0,0 +1,188 @@
+package bcvax.Inventory;
+
+import Utilities.TestListener;
+import bcvaxdevit.my.salesforce.com.Pages.SupplyConsolePage;
+import bcvaxdevit.my.salesforce.com.Pages.Utils;
+import bcvaxdevit.my.salesforce.com.Pages.CommonMethods;
+import bcvax.BaseTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.testng.Assert.assertEquals;
+
+@Listeners({TestListener.class})
+public class Wastage extends BaseTest {
+
+	@Test()
+	public void Can_Do_Single_Wastage_ByDosages_AS_PPHIS() throws Exception {
+		TestcaseID = "223356"; //C223356
+		log("Target Environment: "+ Utils.getTargetEnvironment());
+		int numberOfRows = 1; //Default value, wasting from first row only
+		double amountOfDosesToWaste = 3;
+		log("/*1.----Login as an PPHIS to Supply Console --*/");
+		SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHIS();
+		Thread.sleep(10000);
+		
+		log("/*2.----Validate if Supply Console Page displayed --*/");
+		CommonMethods common = new CommonMethods(getDriver());
+		common.goToSupplyPageIfNeededAndConfirmPageIsDisplayed();
+		
+		log("/*3.----Click on Automation Supply Location_1 --*/");
+		supplyConsolePage.clickOnSupplyLocation_1();
+		Thread.sleep(5000);
+		
+		log("/*4.----Read Remaining Doses And Quantity Before Deduction --*/");
+		HashMap<Integer, ArrayList<Double>> remainingDosesAndQuantityBeforeDeduction = supplyConsolePage.countDosesAndQuantityMap(numberOfRows);
+		
+		log("/*5.----Click on Container's dropdown --*/");
+		supplyConsolePage.clickOnFirstContainerDropDownMenu();
+		Thread.sleep(2000);
+		
+		log("/*6.----select Wastage from the DropDownMenu dropdown menu --*/");
+		supplyConsolePage.selectWastageFromDropDown();
+		double remainingDosesBeforeWastage = supplyConsolePage.getActualRemainingDoses();
+		double doseConversionFactorValue = supplyConsolePage.getDoseConversionFactor();
+		log("/*----Remaining Doses Before Wastage " + remainingDosesBeforeWastage + " --*/");
+		log("/*----Dose Conversion Factor " + doseConversionFactorValue + " --*/");
+		log("/*----Amount Wastage Doses " + amountOfDosesToWaste + " --*/");
+		
+		log("/*7.----set Wastage Doses amount: " +amountOfDosesToWaste +"--*/");
+		supplyConsolePage.setDosesAmount(Double.toString(amountOfDosesToWaste));
+		double remainingDosesAfterWastage = supplyConsolePage.getActualRemainingDoses();
+		log("/*----Quantity Remaining Doses After Wastage " + remainingDosesAfterWastage + " --*/");
+		
+		log("/*8.----Reason For Wastage: 'CCI: Equipment Malfunction' --*/");
+		supplyConsolePage.selectReasonForWastageDropDown();
+		
+		log("/*9----Clicking on btn Wastage --*/");
+		supplyConsolePage.clickBtnWastageAtContainerWastagePopUp();
+		
+		//Verification values in Container - Wastage pop-up
+		assertEquals((remainingDosesBeforeWastage - amountOfDosesToWaste), remainingDosesAfterWastage);
+		
+		log("/*10.----Read Remaining Doses And Quantity After Deduction --*/");
+		HashMap<Integer, ArrayList<Double>> actualRemainingDosesAndQuantityAfterDeduction = supplyConsolePage.countDosesAndQuantityMap(numberOfRows);
+		
+		log("/*11.----Calculating Remaining Doses And Quantity After Deduction --*/");
+		HashMap<Integer, ArrayList<Double>> calculatedRemainingDosesAndQuantityAfterDeduction = new HashMap<>();
+		for (int i = 0; i < remainingDosesAndQuantityBeforeDeduction.size(); i++) {
+			ArrayList<Double> writeToList = new ArrayList<>();
+			ArrayList<Double> readFromList = remainingDosesAndQuantityBeforeDeduction.get(i);
+			double remainingDoses = readFromList.get(0);
+			double remainingQuantity = readFromList.get(1);
+			double doseConversionFactor = readFromList.get(2);
+			//Actual calculation
+			double afterDeductionDoses = remainingDoses - amountOfDosesToWaste;
+			double afterDeductionQuantity = Double.parseDouble(new DecimalFormat("##.####").format(
+					remainingQuantity - (amountOfDosesToWaste / doseConversionFactor)));
+			writeToList.add(afterDeductionDoses);
+			writeToList.add(afterDeductionQuantity);
+			writeToList.add(doseConversionFactor);
+			calculatedRemainingDosesAndQuantityAfterDeduction.put(i, writeToList);
+		}
+		
+		log("/*12.----Compering Remaining Doses and Quantity actual vs calculated--*/");
+		//Comparing 2 objects actualRemainingDosesAndQuantityAfterDeduction vs calculatedRemainingDosesAndQuantityAfterDeduction
+		for (int i = 0; i < actualRemainingDosesAndQuantityAfterDeduction.size(); i++) {
+			ArrayList<Double> afterDeduction = actualRemainingDosesAndQuantityAfterDeduction.get(i);
+			double remainingDosesAfterDeduction = afterDeduction.get(0);
+			double remainingQuantityAfterDeduction = afterDeduction.get(1);
+			double doseConversionFactorBeforeDeduction = afterDeduction.get(2);
+			ArrayList<Double> calculated = calculatedRemainingDosesAndQuantityAfterDeduction.get(i);
+			double calculatedDosesAfterDeduction = calculated.get(0);
+			double calculatedRemainingQuantityAfterDeduction = calculated.get(1);
+			double doseConversionAfterDeduction = calculated.get(2);
+
+			//Comparing results
+			log("Compering remaining doses after adjustment " + remainingDosesAfterDeduction + " vs calculated doses after adjustment " + calculatedDosesAfterDeduction);
+			assertEquals(remainingDosesAfterDeduction, calculatedDosesAfterDeduction);
+
+			log("Compering remaining quantity after adjustment " + remainingQuantityAfterDeduction + " vs calculated quantity after adjustment " + calculatedRemainingQuantityAfterDeduction);
+			assertEquals(remainingQuantityAfterDeduction, calculatedRemainingQuantityAfterDeduction);
+
+			log("Compering dose conversion factor before adjustment " + doseConversionFactorBeforeDeduction + " vs dose conversion factor after adjustment " + doseConversionAfterDeduction);
+			assertEquals(doseConversionFactorBeforeDeduction, doseConversionAfterDeduction);
+
+			log("Compering dose conversion factor value " + doseConversionFactorValue + " vs dose conversion factor after adjustment " + doseConversionAfterDeduction);
+			assertEquals(doseConversionFactorValue, doseConversionAfterDeduction);
+		}
+		
+		log("/*.----Extra steps to check the actual amount of doses wastage on Container - Wastage page --*/");
+		log("/*13.----Click on Container's dropdown --*/");
+		supplyConsolePage.clickOnFirstContainerDropDownMenu();
+		
+		log("/*14.----select Wastage from the DropDownMenu dropdown menu --*/");
+		supplyConsolePage.selectWastageFromDropDown();
+		
+		double actualDosesAmount = supplyConsolePage.getActualRemainingDoses();
+		log("/*----Actual Quantity Doses " + actualDosesAmount + " --*/");
+		assertEquals(actualDosesAmount, remainingDosesAfterWastage);
+	}
+
+	@Test()
+	public void Can_Do_Single_Wastage_ByQuantity_AS_PPHIS() throws Exception {
+		TestcaseID = "223356"; //C223356
+		log("Target Environment: "+ Utils.getTargetEnvironment());
+		int firstRow = 1; //Default value for first row in the grid (Supply container)
+		double amountOfQuantityToWaste = 1;
+		log("/*1.----Login as an PPHIS to Supply Console --*/");
+		SupplyConsolePage supplyConsolePage = loginPage.loginAsPPHIS();
+		Thread.sleep(10000);
+
+		log("/*2.----Validate if Supply Console Page displayed --*/");
+		CommonMethods common = new CommonMethods(getDriver());
+		common.goToSupplyPageIfNeededAndConfirmPageIsDisplayed();
+
+		log("/*3.----Click on Automation Supply Location_1 --*/");
+		supplyConsolePage.clickOnSupplyLocation_1();
+		Thread.sleep(5000);
+
+		log("/*4.----Quantity Remaining Doses/Remaining Quantity check Before --*/");
+		double[] remDosesQtyConversionFactorBefore = common.getRemainingDosesQtyAndConversionFactor(firstRow);
+		double remainingDosesBefore = remDosesQtyConversionFactorBefore[0];
+		log("/*-- . remaining doses Distribution_1_1 After are: -->" + remainingDosesBefore);
+		double remainingQuantitiesBefore = remDosesQtyConversionFactorBefore[1];
+		log("/*-- . remaining Quantity Distribution_1_1 After are: -->" + remainingQuantitiesBefore);
+		double remainingConversionFactor = remDosesQtyConversionFactorBefore[2];
+		log("/*----Dose Conversion Factor " + remainingConversionFactor + " --*/");
+
+		log("/*5.----Click on Container's dropdown --*/");
+		supplyConsolePage.clickOnFirstContainerDropDownMenu();
+		Thread.sleep(2000);
+
+		log("/*6.----select Wastage from the DropDownMenu dropdown menu --*/");
+		supplyConsolePage.selectWastageFromDropDown();
+
+		log("/*7.----set Wastage Quantity amount: " +amountOfQuantityToWaste +"--*/");
+		supplyConsolePage.setQuantityAmount(Double.toString(amountOfQuantityToWaste));
+
+		log("/*8.----Reason For Wastage: 'CCI: Equipment Malfunction' --*/");
+		supplyConsolePage.selectReasonForWastageDropDown();
+
+		log("/*9----Clicking on btn Wastage --*/");
+		supplyConsolePage.clickBtnWastageAtContainerWastagePopUp();
+
+		log("/*10.----Quantity Remaining Doses/Remaining Quantity check After --*/");
+		double[] remDosesQtyConversionFactorAfter = common.getRemainingDosesQtyAndConversionFactor(firstRow);
+		double remainingDosesAfter = remDosesQtyConversionFactorAfter[0];
+		log("/*-- . remaining doses Distribution_1_1 After are: -->" + remainingDosesAfter);
+		double remainingQuantitiesAfter = remDosesQtyConversionFactorAfter[1];
+		log("/*-- . remaining Quantity Distribution_1_1 After are: -->" + remainingQuantitiesAfter);
+		double remainingConversionAfter = remDosesQtyConversionFactorAfter[2];
+		log("/*----Dose Conversion Factor " + remainingConversionAfter + " --*/");
+
+		log("/*11.----Validate Remaining Doses, Remaining Quantities and Conversion factor --*/");
+		log("----Validation by Doses --");
+		double remainingDosesBeforeCalculation = Double.parseDouble(new DecimalFormat("##.####").format(
+				(remainingQuantitiesBefore - amountOfQuantityToWaste) * remainingConversionFactor));
+		assertEquals(remainingDosesBeforeCalculation, remainingDosesAfter);
+		log("----Validation by Quantities --");
+		assertEquals((remainingQuantitiesBefore - amountOfQuantityToWaste), remainingQuantitiesAfter);
+		log("----Validation Conversion factor --");
+		assertEquals(remainingConversionFactor,remainingConversionAfter);
+	}
+}
---
+++ src/test/java/bcvax/tests/Precondition/CitizenDuplicates.java
@@ -0,0 +1,155 @@
+package bcvax.Precondition;
+
+import Utilities.TestListener;
+import bcvaxdevit.my.salesforce.com.Pages.Utils;
+import bcvax.BaseTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import bcvaxdevit.my.salesforce.com.Pages.InClinicExperiencePage;
+
+@Listeners({TestListener.class})
+public class CitizenDuplicates extends BaseTest {
+	
+	@Test(testName = "Login to InClinicExperience as Clinician and search for Dupls BCVAXDEVIT")
+	public void Can_search_and_remove_Citizen_participant_account() throws Exception {
+		TestcaseID = "219865"; //C219865
+		log("Target Environment: "+ Utils.getTargetEnvironment());
+		log("Searching and Removing Citizen Duplicates BCVAXDEVIT");
+		/*----Login as an Clinician In-Clinic Experience --*/
+		log("/*----Login as an Clinician In-Clinic Experience --*/");
+		InClinicExperiencePage inClinicExperiencePage = loginPage.loginAsPrecondition();
+		if (inClinicExperiencePage.displayIceApp()) {
+			log("/*---- User already on ICE--*/");
+		} else {
+			log("/*---- Navigate to ICE APP --*/");
+			inClinicExperiencePage.selectIceApp();
+			Thread.sleep(2000);
+		}
+		/*----Go to Register Tab ---*/
+		log("/*----Go to Register Tab ---*/");
+		inClinicExperiencePage.clickRegisterTab();
+		Thread.sleep(5000);
+		/*----Search for Participant account ---*/
+		log("/*----Search for Participant account ---*/");
+		inClinicExperiencePage.SearchForCitizen("Ludovika BCVaxLimeburn");
+		log("/*----Search for Ludovika is Successful ---*/");
+		if (!inClinicExperiencePage.userFound()) {
+			log("/*----User --> Ludovika not found and return---*/");
+		}
+		while (inClinicExperiencePage.userFound()) {
+			log("/*----User found and Navigated to record page ---*/");
+			Thread.sleep(2000);
+			inClinicExperiencePage.clickRelatedTab();
+			log("/*---- Navigated to Person Account related tab ---*/");
+			Thread.sleep(2000);
+			if (!inClinicExperiencePage.selectImmsRecord()) {
+				log("/*----No Imms Record found and return---*/");
+			} else {
+				log("/*---- User navigated to Imms record ---*/");
+				Thread.sleep(2000);
+				inClinicExperiencePage.deleteImmsRecord();
+				log("/*---- Imms record deleted Successfully ---*/");
+				Thread.sleep(2000);
+			}
+			inClinicExperiencePage.clickRelatedTab();
+			log("/*---- Navigate back to Person Account related tab after deleting imms record---*/");
+			Thread.sleep(5000);
+			if (!inClinicExperiencePage.selectRERNRecord()) {
+				log("/*----No RERN Record found and return---*/");
+			} else {
+				log("/*---- User navigated to RERN record ---*/");
+				Thread.sleep(2000);
+				inClinicExperiencePage.deleteRERNRecord();
+				log("/*---- RERN record deleted Successfully ---*/");
+				Thread.sleep(2000);
+			}
+			log("/*---- Navigated to Person Account related tab ---*/");
+			inClinicExperiencePage.deletePersonAccount();
+			log("/*---- Person Account deleted Successfully ---*/");
+			Thread.sleep(2000);
+			inClinicExperiencePage.clickRegisterTab();
+			Thread.sleep(5000);
+			inClinicExperiencePage.closeOpenTabs();
+			log("/*---- Close the deleted Person Account ---*/");
+			Thread.sleep(2000);
+			log("/*----Re Searching for the Participant account ---*/");
+			inClinicExperiencePage.SearchForCitizen("Ludovika BCVaxLimeburn");
+			log("/*----Search for Ludovika is Successful ---*/");
+		}
+	}
+
+
+	public void UPDATED_Pre_conditions_step_Remove_Dups_Citizen_participant_account() throws Exception {
+		String legalFirstName = "Anne-marie";
+		String legalLastName = "BCVaxJacketts";
+		String legalMiddleName = "Elissa";
+
+		TestcaseID = "219865";
+		log("Target Environment: "+ Utils.getTargetEnvironment());
+		log("Searching and Removing Citizen Duplicates ");
+		log("/*----Login as an Clinician In-Clinic Experience --*/");
+		InClinicExperiencePage inClinicExperiencePage = loginPage.loginAsPrecondition();
+		if (inClinicExperiencePage.displayIceApp()) {
+			System.out.println("/*---- User already on ICE--*/");
+		} else {
+			System.out.println("/*---- Navigate to ICE APP --*/");
+			inClinicExperiencePage.selectIceApp();
+			Thread.sleep(2000);
+		}
+		/*----Go to Register Tab ---*/
+		log("/*----Go to Register Tab ---*/");
+		inClinicExperiencePage.clickRegisterTab();
+		Thread.sleep(5000);
+		/*----Search for Participant account ---*/
+		log("/*----Search for Participant account ---*/");
+		inClinicExperiencePage.SearchForCitizen(legalFirstName + " " + legalLastName);
+		log("/*----Search for " + legalFirstName + " " + legalLastName + " is Successful ---*/");
+		if (!inClinicExperiencePage.userFoundWithParameters(legalFirstName, legalMiddleName, legalLastName)) {
+			log("/*----User --> " + legalFirstName + " " + legalLastName + " not found and return---*/");
+		}
+		while (inClinicExperiencePage.userFoundWithParameters(legalFirstName, legalMiddleName, legalLastName)) {
+			log("/*----User found and Navigated to record page ---*/");
+			Thread.sleep(2000);
+			inClinicExperiencePage.clickRelatedTab();
+			log("/*---- Navigated to Person Account related tab ---*/");
+			Thread.sleep(7000);
+			if (!inClinicExperiencePage.selectImmsRecord()) {
+				log("/*----No Imms Record found and return---*/");
+			} else {
+				log("/*---- User navigated to Imms record ---*/");
+				Thread.sleep(2000);
+				inClinicExperiencePage.deleteImmsRecord();
+				System.out.println("/*---- Imms record deleted Successfully ---*/");
+				Thread.sleep(2000);
+			}
+			inClinicExperiencePage.clickRelatedTab();
+			log("/*---- Navigate back to Person Account related tab after deleting imms record---*/");
+			Thread.sleep(5000);
+			if (!inClinicExperiencePage.selectRERNRecord()) {
+				log("/*----No RERN Record found and return---*/");
+			} else {
+				log("/*---- User navigated to RERN record ---*/");
+				Thread.sleep(2000);
+				inClinicExperiencePage.deleteRERNRecord();
+				log("/*---- RERN record deleted Successfully ---*/");
+				Thread.sleep(5000);
+			}
+			log("/*---- Navigated to Person Account related tab ---*/");
+			inClinicExperiencePage.deletePersonAccount();
+			log("/*---- Person Account deleted Successfully ---*/");
+			Thread.sleep(2000);
+			inClinicExperiencePage.clickRegisterTab();
+			Thread.sleep(5000);
+			inClinicExperiencePage.closeOpenTabs();
+			log("/*---- Close the deleted Person Account ---*/");
+			Thread.sleep(2000);
+			log("/*----Re Searching for the Participant account ---*/");
+			inClinicExperiencePage.SearchForCitizen(legalFirstName + " " + legalLastName);
+			log("/*----Search for " + legalFirstName + " " + legalLastName + " is Successful ---*/");
+		}
+	}
+	
+}
+
+
+
