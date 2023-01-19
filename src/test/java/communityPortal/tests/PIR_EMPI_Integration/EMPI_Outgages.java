26549ba4ba16bc6ed15a1b973a7ebcac92af9b8c
igor.emelyanov <igor.emelyanov@phsa.ca>
2023-01-19 11:37:52 -0800

    some comments to Integration test suits

---
+++ src/test/java/communityPortal/tests/PIR_EMPI_Integration/EMPI_Outgages.java
@@ -0,0 +1,11 @@
+package communityPortal.tests.PIR_EMPI_Integration;
+
+/*
+If 1.I can Register my Citizen (via Citizen Portal)
+in imms but can not make PNH Verified green
+2. and I can not Register via admin side at all "Match Unsuccesful"
+seems PIR is ok but EMPI has down
+  ---EMPI ENV_5_2 HIAT Health Ministry Outgages
+*/
+public class EMPI_Outgages {
+}
---
+++ src/test/java/communityPortal/tests/PIR_EMPI_Integration/PIR_account_auto_linking.java
@@ -0,0 +1,22 @@
+package communityPortal.tests.PIR_EMPI_Integration;
+
+/*
+If after Registration new Citizen(no PIR no Participant acc in imms)
+PIR account has not linked to the Participant one automatically:
+1.popup message "This client profile is not linked to a PIR record"
+2.and no PIR Account number in Person Account Details
+but EMPI Verification is green
+that's meaning that we lost PIR integration
+(Panorama has down or certificates expired issue)
+*/
+
+/*
+If During the Registration new Citizen popup message like
+"Match Unsuccesful"
+PIR is down but EMPI might work.
+ */
+
+
+
+public class PIR_account_auto_linking {
+}
