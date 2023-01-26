f0a4f0d6791c3cf7634a1a3c4a31eee0d6495206
Roman <roman.shevtsov@phsa.ca>
2023-01-26 15:16:34 -0800

    added missed class

---
+++ src/main/java/bcvax/pages/WaitConditions.java
@@ -0,0 +1,31 @@
+package bcvax.pages;
+
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.util.List;
+
+public class WaitConditions {
+
+    public static ExpectedCondition<Boolean> textToBePresentInElements(final List<WebElement> elements, final String text) {
+        return new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                try {
+                    String elementText = "";
+                    for (WebElement e : elements) {
+                        elementText = e.getText();
+                    }
+                    return elementText.contains(text);
+                } catch (StaleElementReferenceException var3) {
+                    return false;
+                }
+            }
+
+            public String toString() {
+                return String.format("text ('%s') to be present in elements %s", text, elements);
+            }
+        };
+    }
+}
