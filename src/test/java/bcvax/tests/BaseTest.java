69f91aa7257641170a2482bac9a0b9e5eb532533
DmytroB <dmytro.bilous@phsa.ca>
2023-03-03 09:42:51 -0800

    added functinality to run as immsAdmin

--- src/test/java/bcvax/tests/BaseTest.java
+++ src/test/java/bcvax/tests/BaseTest.java
@@ -56,7 +56,7 @@ public class BaseTest {
 			log("Test Rail was not updated: "+e);
 		}
 		driver.manage().deleteAllCookies();
-		driver.close();
+		driver.quit();
 	}
 	
 	public WebDriver getDriver() {
