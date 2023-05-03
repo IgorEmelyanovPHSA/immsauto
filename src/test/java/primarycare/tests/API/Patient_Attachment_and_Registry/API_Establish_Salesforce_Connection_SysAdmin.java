af61c44851b6ed4c8c357f8f598125c0fadae7db
igor.emelyanov <igor.emelyanov@phsa.ca>
2023-05-03 16:11:26 -0700

    adding TC id for API reporting

--- src/test/java/primarycare/tests/API/Patient_Attachment_and_Registry/API_Establish_Salesforce_Connection_SysAdmin.java
+++ src/test/java/primarycare/tests/API/Patient_Attachment_and_Registry/API_Establish_Salesforce_Connection_SysAdmin.java
@@ -3,13 +3,15 @@ package primarycare.tests.API.Patient_Attachment_and_Registry;
 import org.testng.annotations.Test;
 import static primarycare.tests.BaseTest_PrimaryCare.log;
 import primarycare.pages.APIEstablishSFConnection;
+import primarycare.tests.BaseTest_PrimaryCare;
 
-public class API_Establish_Salesforce_Connection_SysAdmin {
+public class API_Establish_Salesforce_Connection_SysAdmin extends BaseTest_PrimaryCare {
 
     //public String access_token;
 
     @Test
     public void API_Can_Establish_Salesforce_connection_as_SysAdmin(){
+        TestcaseID = "252563"; //C252563
         APIEstablishSFConnection sfConnection = new APIEstablishSFConnection();
         String acc_token = sfConnection.establishConnection();
         log("Connection to SF successful!");
