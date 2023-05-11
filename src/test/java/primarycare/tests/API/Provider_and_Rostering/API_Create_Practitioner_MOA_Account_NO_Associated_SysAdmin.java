f51b24b032cb85eadff23f9192e6e7c6ed21ef76
igor.emelyanov <igor.emelyanov@phsa.ca>
2023-05-11 14:48:29 -0700

    adding a skeleton for API Practitioner Creating /Removal Account

---
+++ src/test/java/primarycare/tests/API/Provider_and_Rostering/API_Create_Practitioner_MOA_Account_NO_Associated_SysAdmin.java
@@ -0,0 +1,48 @@
+package primarycare.tests.API.Provider_and_Rostering;
+
+import org.testng.annotations.Test;
+import primarycare.pages.APICreatePatientAccount;
+import primarycare.pages.APIDelete;
+import primarycare.tests.BaseTest_PrimaryCare;
+
+public class API_Create_Practitioner_MOA_Account_NO_Associated_SysAdmin extends BaseTest_PrimaryCare {
+    //public String lastname = "IgorAPI_Account_" + new Random().nextInt(1000);
+    public String firstName = "Kenton3";
+    public String lastName = "Troup3";
+    public String recordTypeId = "0125f0000003ekBAAQ ";
+    public String phn = "9873010088";
+    public String gender = "Male";
+    public String birthdate = "1959-12-05";
+    public String preferredCommunicationChannel = "Email";
+    public String mobile = "7788793897";
+    public String email = "accountToDelete@phsa.ca";
+    public String street = "307-7631 Francis Rd";
+    public String city = "Richmond";
+    public String postalcode = "V6Y 1A3";
+    public String isActive = "true";
+
+    public String accId;
+
+    @Test(priority = 1)
+    public void API_Can_Create_Practitioner_MOA_Account_No_Associated_in_Salesforce_as_SysAdmin(){
+        TestcaseID = "252884"; //C252884
+        APICreatePatientAccount apiinsertAccount = new APICreatePatientAccount();
+        log("Create Patient account record.");
+        String accountID = apiinsertAccount.insertAccount(recordTypeId, firstName,lastName,phn,gender,
+                birthdate,preferredCommunicationChannel, mobile,email,street,city,postalcode,isActive);
+        log("Created Patient's id is: " +accountID);
+        accId=accountID;
+    }
+
+
+    @Test(priority = 2)
+    public void API_Remove_Create_Practitioner_MOA_Account_No_Associated_in_Salesforce_as_SysAdmin(){
+        TestcaseID = "252885"; //C252885
+        APIDelete apidelete = new APIDelete();
+        log("Delete account from Account.");
+        String apiResponse = apidelete.deleteAccount(accId);
+        log("Deleted Account from Account is: " +accId);
+        log(apiResponse);
+        //Assert.assertEquals(accountNameReturned, name);
+    }
+}
---
+++ src/test/java/primarycare/tests/API/Provider_and_Rostering/API_Create_Practitioner_MOA_Facility_Associated_SysAdmin.java
@@ -0,0 +1,4 @@
+package primarycare.tests.API.Provider_and_Rostering;
+
+public class API_Create_Practitioner_MOA_Facility_Associated_SysAdmin {
+}
