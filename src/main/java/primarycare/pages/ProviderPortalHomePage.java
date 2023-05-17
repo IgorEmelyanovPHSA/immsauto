c321a8ea5415f265f3eadd88d170ae8190b3f540
igor.emelyanov <igor.emelyanov@phsa.ca>
2023-05-17 15:49:18 -0700

    adding assertions

--- src/main/java/primarycare/pages/ProviderPortalHomePage.java
+++ src/main/java/primarycare/pages/ProviderPortalHomePage.java
@@ -17,6 +17,36 @@ public class ProviderPortalHomePage extends BasePage{
     private WebElement view_link;
     private By view_link_1 = By.xpath("(//span[text()='View'])[2]");
 
+    @FindBy(xpath = "//div[@title='Edit']")
+    private WebElement edit_button;
+    private By edit_button_1 = By.xpath("//div[@title='Edit']");
+
+    @FindBy(xpath = "(//input[@data-interactive-lib-uid])[1]")
+    private WebElement desired_panel_size_component;
+    private By desired_panel_size_component_1 = By.xpath("(//input[@data-interactive-lib-uid])[1]");
+
+    @FindBy(xpath = "//a[@class='select']")
+    private WebElement accepting_new_patient_dropdown;
+    private By accepting_new_patient_dropdown_1 = By.xpath("//a[@class='select']");
+
+    @FindBy(xpath = "//a[@aria-disabled][@title='Yes']")
+    private WebElement select_yes_from_accepting_new_patient_options;
+    private By select_yes_from_accepting_new_patient_options_1 = By.xpath("//a[@aria-disabled][@title='Yes']");
+
+
+    @FindBy(xpath = "(//input[@data-interactive-lib-uid])[2]")
+    private WebElement max_new_requests_component;
+    private By max_new_requests_component_1 = By.xpath("(//input[@data-interactive-lib-uid])[2]");
+
+    @FindBy(xpath = "//div[@class='modal-footer slds-modal__footer']//span[text()='Save']")
+    private WebElement save_button;
+    private By save_button_1 = By.xpath("//div[@class='modal-footer slds-modal__footer']//span[text()='Save']");
+
+    @FindBy(xpath = "(//span[@class='uiOutputNumber'])[1]")
+    private WebElement desired_panel_size_actual_field_value;
+    private By desired_panel_size_actual_field_value_1 = By.xpath("(//span[@class='uiOutputNumber'])[1]");
+
+
     /*---------Constructor-------*/
     public ProviderPortalHomePage(WebDriver driver) {
         super(driver);
@@ -47,6 +77,60 @@ public class ProviderPortalHomePage extends BasePage{
         waitForElementToBeVisible(driver, view_link, 10);
         Thread.sleep(1000);
         view_link.click();
+    }
+
+    public void clickEdit() throws InterruptedException {
+        waitForElementToBeVisible(driver, edit_button, 10);
+        Thread.sleep(1000);
+        edit_button.click();
+    }
+
+    public void enterDesiredPanelSize(int desiredPanelSize) throws InterruptedException {
+        waitForElementToBeLocated(driver, desired_panel_size_component_1, 10);
+        Thread.sleep(1000);
+        desired_panel_size_component.click();
+        Thread.sleep(1000);
+        desired_panel_size_component.clear();
+        Thread.sleep(1000);
+        //String size ="" + String desiredPanelSize;
+        String size = Integer.toString(desiredPanelSize);
+        desired_panel_size_component.sendKeys(size);
+        Thread.sleep(1000);
+    }
+
+    public void selectAcceptingNewPatientsOption(String isAcceptingNewPatients) throws InterruptedException {
+        waitForElementToBeLocated(driver, accepting_new_patient_dropdown_1, 10);
+        Thread.sleep(1000);
+        accepting_new_patient_dropdown.click();
+        Thread.sleep(1000);
+        waitForElementToBeLocated(driver, select_yes_from_accepting_new_patient_options_1, 10);
+        Thread.sleep(1000);
+        select_yes_from_accepting_new_patient_options.click();
+        Thread.sleep(1000);
+    }
+
+    public void enterMaxNewRequests(String maxNewRequests) throws InterruptedException {
+        waitForElementToBeLocated(driver, max_new_requests_component_1, 10);
+        Thread.sleep(1000);
+        max_new_requests_component.click();
+        Thread.sleep(1000);
+        max_new_requests_component.clear();
+        Thread.sleep(1000);
+        max_new_requests_component.sendKeys(maxNewRequests);
+        Thread.sleep(1000);
+    }
+
+    public void clickSave() throws InterruptedException {
+        waitForElementToBeVisible(driver, save_button, 10);
+        Thread.sleep(1000);
+        save_button.click();
+    }
+
+    public String getDesiredPanelSizeActualForValidation() throws InterruptedException {
+        waitForElementToBeLocated(driver, desired_panel_size_actual_field_value_1, 10);
+        Thread.sleep(2000);
+        desired_panel_size_actual_field_value.isDisplayed();
+        return (desired_panel_size_actual_field_value.getText());
     }
 
 }
--- src/test/java/primarycare/tests/Selenium/Provider_Portal/Portal_Accepting_New_Patients_And_Max_New_Requests.java
+++ src/test/java/primarycare/tests/Selenium/Provider_Portal/Portal_Accepting_New_Patients_And_Max_New_Requests.java
@@ -7,6 +7,11 @@ import primarycare.pages.ProviderPortalHomePage;
 import primarycare.tests.BaseTest_PrimaryCare;
 import primarycare.tests.Utilities.TestListener;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
+import static org.testng.Assert.assertEquals;
+
 @Listeners({TestListener.class})
 public class Portal_Accepting_New_Patients_And_Max_New_Requests extends BaseTest_PrimaryCare {
     private String clinicName_1 = "CASTLEGAR MEDICAL CLINIC ASSOCIATES";
@@ -17,6 +22,13 @@ public class Portal_Accepting_New_Patients_And_Max_New_Requests extends BaseTest
     //private String isPrimary = "True"; is it Primary for Practitioner?
 
     //private String clinic_Name_2 = "CASTLEGAR MEDICAL CLINIC11111";
+
+    //private String desiredPanelSize = "77777";
+    private int desiredPanelSize = ((1 + new Random().nextInt(2)) * 10000) + new Random().nextInt(10000);
+    private String isAcceptingNewPatients = "Yes";
+    //private String maxNewRequests = "88888";
+    int maxNew = new Random().nextInt(100000);
+    String maxNewRequest = String.format("%05d", maxNew);
 
 
     @Test(priority = 1)
@@ -38,6 +50,37 @@ public class Portal_Accepting_New_Patients_And_Max_New_Requests extends BaseTest
         log("/*3.----Click View in CASTLEGAR Medical Associated Facility Panel --*/");
         providerPortalHomePage.clickView();
         Thread.sleep(1000);
+
+        log("/*4.----Click Edit --*/");
+        providerPortalHomePage.clickEdit();
+        Thread.sleep(1000);
+
+        log("/*5.----Enter random Desired Panel Size: " + desiredPanelSize + "--*/");
+        providerPortalHomePage.enterDesiredPanelSize(desiredPanelSize);
+        Thread.sleep(1000);
+
+        log("/*6.----Accepting new Patients: " + isAcceptingNewPatients + "--*/");
+        providerPortalHomePage.selectAcceptingNewPatientsOption(isAcceptingNewPatients);
+        Thread.sleep(1000);
+
+        log("/*7.----Enter random Max New Requests: " + maxNewRequest + "--*/");
+        providerPortalHomePage.enterMaxNewRequests(maxNewRequest);
+        Thread.sleep(1000);
+
+        log("/*8.----Click Save --*/");
+        providerPortalHomePage.clickSave();
+        Thread.sleep(1000);
+
+        log("/*9.---- Validate Desired Panel Size  ---*/");
+        //String desiredPanelSizeExpected = String.format("%,.2f", desiredPanelSize);
+        //String desiredPanelSizeExpected = Double.parseDouble(new DecimalFormat("##,##").format(desiredPanelSize));
+        //double remainingDoses_after_Calculation_Lot_EK4241_Distribution_1_2 =
+                //Double.parseDouble(new DecimalFormat("##.##").
+                        //format((remainingDoses_before_Lot_EK4241_Distribution_1_2 + 10)));
+        String desiredPanelSizeActual = providerPortalHomePage.getDesiredPanelSizeActualForValidation();
+        log("/*---Desired Panel Size actual is: " + desiredPanelSizeActual + " --*/");
+        //assertEquals(desiredPanelSizeExpected, desiredPanelSizeActual);
+        Thread.sleep(2000);
 
     }
 
