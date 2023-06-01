6c53218809686a35ef9313079e91e3458663e107
Igor Chen <igorchen@icloud.com>
2023-06-01 13:50:40 -0700

    Fixed missing method in org

--- src/test/java/bcvax/tests/UserDefaults/UserDefaultsSettingsValidation.java
+++ src/test/java/bcvax/tests/UserDefaults/UserDefaultsSettingsValidation.java
@@ -84,7 +84,7 @@ public class UserDefaultsSettingsValidation extends BaseTest {
         log("/*9.3----Go to Supply Locations Tab --*/");
         supplyConsolePage.clickSupplyLocationsTab();
         log("/*9.4----Click on Automation Supply Location_1 --*/");
-        supplyConsolePage.clickOnSupplyLocation(clinicLocation);
+        supplyConsolePage.selectSupplyLocationName(clinicLocation);
 
         //common.goToSupplyPageIfNeededAndConfirmPageIsDisplayedNew();
 
