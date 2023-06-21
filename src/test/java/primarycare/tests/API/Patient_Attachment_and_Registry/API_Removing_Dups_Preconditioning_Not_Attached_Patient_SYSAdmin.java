e56f7362651d397b5c6241a8e6fbd52d4747a03e
igor.emelyanov <igor.emelyanov@phsa.ca>
2023-06-21 12:40:32 -0700

    refactoring for API removing Dups

--- src/test/java/primarycare/tests/API/Patient_Attachment_and_Registry/API_Removing_Dups_Preconditioning_Not_Attached_Patient_SYSAdmin.java
+++ src/test/java/primarycare/tests/API/Patient_Attachment_and_Registry/API_Removing_Dups_Preconditioning_Not_Attached_Patient_SYSAdmin.java
@@ -6,11 +6,9 @@ import primarycare.tests.API_BaseTest_PrimaryCare;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import javax.swing.text.Utilities;
-
 
 @Listeners({TestListener.class})
-public class API_Removing_Not_Attached_Patient_SYSAdmin extends API_BaseTest_PrimaryCare {
+public class API_Removing_Dups_Preconditioning_Not_Attached_Patient_SYSAdmin extends API_BaseTest_PrimaryCare {
     private String legalFirstName = "Sandy";
     private String legalLastName = "Prior";
     private String dateOfBirth = "March 1, 1975";
@@ -21,7 +19,7 @@ public class API_Removing_Not_Attached_Patient_SYSAdmin extends API_BaseTest_Pri
 
 
     @Test(priority = 1)
-    public void API_Can_Remove_Patient_from_SF_as_an_SYSAdmin() throws Exception {
+    public void API_Can_Remove_Duplicates_Preconditioning_Not_Attached_Patient_SYSAdmin() throws Exception {
         TestcaseID = "251774"; //C251774
         log("/---API call to remove duplicate Patient account and Case if found--*/");
         ApiQueries.apiCallToRemovePatientAccount(email, legalLastName, legalFirstName);
