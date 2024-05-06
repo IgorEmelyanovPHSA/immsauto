377694bdf11606acdc001cc082697b4a1086ba14
Igor Chen <igorchen@icloud.com>
2024-05-05 18:27:09 -0700

    Updated password

--- src/test/java/bcvax/tests/AddAppointmentDays.java
+++ src/test/java/bcvax/tests/AddAppointmentDays.java
@@ -24,8 +24,8 @@ public class AddAppointmentDays extends BaseTest {
         testData = Utils.getTestData(env);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        LocalDate start_date = LocalDate.parse("2024-04-29", dtf);
-        LocalDate end_date = LocalDate.parse("2024-05-05", dtf);
+        LocalDate start_date = LocalDate.parse("2024-05-06", dtf);
+        LocalDate end_date = LocalDate.parse("2024-05-12", dtf);
         ArrayList<String> appointment_dates = new ArrayList();
         for (LocalDate my_appointment_date = start_date; !my_appointment_date.isAfter(end_date); my_appointment_date = my_appointment_date.plusDays(1))
         {
--- src/test/java/bcvax/tests/InClinicExperience/E2E_Deferrals.java
+++ src/test/java/bcvax/tests/InClinicExperience/E2E_Deferrals.java
@@ -26,13 +26,13 @@ public class E2E_Deferrals extends BaseTest {
         test_data.setPostalCode("V3L5L2");
         test_data.setPersonalHealthNumber("9746170911");
         test_data.setEmail("accountToDelete@phsa.ca");
-        test_data.setEnv(String.valueOf(testData.get("supplyLocationConsumption")));
-        test_data.setDose(String.valueOf(testData.get("administration_dose")));
-        test_data.setLot(String.valueOf(testData.get("administration_lot")));
+        test_data.setClinicName(String.valueOf(testData.get("supplyLocationConsumption")));
+        test_data.setDose(String.valueOf(testData.get("covidDose")));
+        test_data.setLot(String.valueOf(testData.get("covidLot")));
         test_data.setRoute(String.valueOf(testData.get("routeConsumption")));
         test_data.setSite(String.valueOf(testData.get("siteConsumption")));
         test_data.setConsentProvider(String.valueOf(testData.get("consentProvider")));
-        test_data.setAgent(String.valueOf(testData.get("administration_agent")));
+        test_data.setAgent(String.valueOf(testData.get("vaccineAgent")));
         log("/*1.----Login --*/");
         try {
             LoginPage.loginAsImmsBCAdmin(driver);
