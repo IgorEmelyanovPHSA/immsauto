346205f4524211723930c5ca7a579c345ae57070
igorchenphsa <igor.chen@phsa.ca>
2024-05-15 08:22:55 -0700

    Added igorauto env config

--- src/test/java/bcvax/tests/AddAppointmentDays.java
+++ src/test/java/bcvax/tests/AddAppointmentDays.java
@@ -24,7 +24,7 @@ public class AddAppointmentDays extends BaseTest {
         testData = Utils.getTestData(env);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        LocalDate start_date = LocalDate.parse("2024-05-07", dtf);
+        LocalDate start_date = LocalDate.parse("2024-05-09", dtf);
         LocalDate end_date = LocalDate.parse("2024-05-12", dtf);
         ArrayList<String> appointment_dates = new ArrayList();
         for (LocalDate my_appointment_date = start_date; !my_appointment_date.isAfter(end_date); my_appointment_date = my_appointment_date.plusDays(1))
