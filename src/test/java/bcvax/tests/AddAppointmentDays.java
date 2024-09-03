39e7ec9910a447b0497393769b9715a1a8d3c4fb
Igor Chen <igorchen@icloud.com>
2024-09-03 08:18:07 -0700

    Updated passwords in devit

--- src/test/java/bcvax/tests/AddAppointmentDays.java
+++ src/test/java/bcvax/tests/AddAppointmentDays.java
@@ -24,8 +24,8 @@ public class AddAppointmentDays extends BaseTest {
         testData = Utils.getTestData(env);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        LocalDate start_date = LocalDate.parse("2024-08-28", dtf);
-        LocalDate end_date = LocalDate.parse("2024-09-01", dtf);
+        LocalDate start_date = LocalDate.parse("2024-09-02", dtf);
+        LocalDate end_date = LocalDate.parse("2024-09-08", dtf);
         ArrayList<String> appointment_dates = new ArrayList();
         for (LocalDate my_appointment_date = start_date; !my_appointment_date.isAfter(end_date); my_appointment_date = my_appointment_date.plusDays(1))
         {
